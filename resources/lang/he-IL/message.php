4fe42f5d2f4abe750bf8b03dc13d93f426631ea1
Brady Wetherington <uberbrady@gmail.com>
2022-01-13 22:07:26 -0800

    New translations message.php (Hebrew) Fixed: New translations message.php from Crowdin

--- resources/lang/he-IL/message.php
+++ resources/lang/he-IL/message.php
@@ -2,21 +2,26 @@
 
 return array(
 
+    'does_not_exist' => 'Supplier does not exist.',
+
+
+    'create' => array(
+        'error'   => 'Supplier was not created, please try again.',
+        'success' => 'Supplier created successfully.'
+    ),
 
     'update' => array(
-        'error'                 => 'An error has occurred while updating. ',
-        'success'               => 'Settings updated successfully.'
+        'error'   => 'Supplier was not updated, please try again',
+        'success' => 'Supplier updated successfully.'
     ),
-    'backup' => array(
-        'delete_confirm'        => 'Are you sure you would like to delete this backup file? This action cannot be undone. ',
-        'file_deleted'          => 'The backup file was successfully deleted. ',
-        'generated'             => 'A new backup file was successfully created.',
-        'file_not_found'        => 'That backup file could not be found on the server.',
-    ),
-    'purge' => array(
-        'error'     => 'An error has occurred while purging. ',
-        'validation_failed'     => 'Your purge confirmation is incorrect. Please type the word "DELETE" in the confirmation box.',
-        'success'               => 'Deleted records successfully purged.'
-    ),
+
+    'delete' => array(
+        'confirm'   => 'Are you sure you wish to delete this supplier?',
+        'error'   => 'There was an issue deleting the supplier. Please try again.',
+        'success' => 'Supplier was deleted successfully.',
+        'assoc_assets'	 => 'This supplier is currently associated with :asset_count asset(s) and cannot be deleted. Please update your assets to no longer reference this supplier and try again. ',
+        'assoc_licenses'	 => 'This supplier is currently associated with :licenses_count licences(s) and cannot be deleted. Please update your licenses to no longer reference this supplier and try again. ',
+        'assoc_maintenances'	 => 'This supplier is currently associated with :asset_maintenances_count asset maintenances(s) and cannot be deleted. Please update your asset maintenances to no longer reference this supplier and try again. ',
+    )
 
 );
