ad5ace0ac061ca5b8a50b7a15f95dc8fca8830f4
Brady Wetherington <uberbrady@gmail.com>
2022-01-14 00:52:56 -0800

    New translations message.php (Vietnamese) Fixed: New translations message.php from Crowdin

--- resources/lang/vi-VN/message.php
+++ resources/lang/vi-VN/message.php
@@ -2,36 +2,24 @@
 
 return array(
 
-    'does_not_exist' => 'The accessory [:id] does not exist.',
-    'assoc_users'	 => 'This accessory currently has :count items checked out to users. Please check in the accessories and and try again. ',
+    'does_not_exist' => 'Depreciation class does not exist.',
+    'assoc_users'	 => 'This depreciation is currently associated with one or more models and cannot be deleted. Please delete the models, and then try deleting again. ',
+
 
     'create' => array(
-        'error'   => 'The accessory was not created, please try again.',
-        'success' => 'The accessory was successfully created.'
+        'error'   => 'Depreciation class was not created, please try again. :(',
+        'success' => 'Depreciation class created successfully. :)'
     ),
 
     'update' => array(
-        'error'   => 'The accessory was not updated, please try again',
-        'success' => 'The accessory was updated successfully.'
+        'error'   => 'Depreciation class was not updated, please try again',
+        'success' => 'Depreciation class updated successfully.'
     ),
 
     'delete' => array(
-        'confirm'   => 'Are you sure you wish to delete this accessory?',
-        'error'   => 'There was an issue deleting the accessory. Please try again.',
-        'success' => 'The accessory was deleted successfully.'
-    ),
-
-     'checkout' => array(
-        'error'   		=> 'Accessory was not checked out, please try again',
-        'success' 		=> 'Accessory checked out successfully.',
-        'user_does_not_exist' => 'That user is invalid. Please try again.'
-    ),
-
-    'checkin' => array(
-        'error'   		=> 'Accessory was not checked in, please try again',
-        'success' 		=> 'Accessory checked in successfully.',
-        'user_does_not_exist' => 'That user is invalid. Please try again.'
+        'confirm'   => 'Are you sure you wish to delete this depreciation class?',
+        'error'   => 'There was an issue deleting the depreciation class. Please try again.',
+        'success' => 'The depreciation class was deleted successfully.'
     )
-
 
 );
