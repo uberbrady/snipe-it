bc20fe8a362b3287526dbd3352f57d8ec9a48c58
Brady Wetherington <uberbrady@gmail.com>
2022-01-13 21:48:08 -0800

    New translations message.php (Danish) Fixed: New translations message.php from Crowdin

--- resources/lang/da-DK/message.php
+++ resources/lang/da-DK/message.php
@@ -1,25 +1,18 @@
 <?php
-
 return array(
-
-    'does_not_exist' => 'Category does not exist.',
-    'assoc_models'	 => 'This category is currently associated with at least one model and cannot be deleted. Please update your models to no longer reference this category and try again. ',
-    'assoc_items'	 => 'This category is currently associated with at least one :asset_type and cannot be deleted. Please update your :asset_type  to no longer reference this category and try again. ',
-
+    'does_not_exist' => 'Company does not exist.',
+    'assoc_users'    => 'This company is currently associated with at least one model and cannot be deleted. Please update your models to no longer reference this company and try again. ',
     'create' => array(
-        'error'   => 'Category was not created, please try again.',
-        'success' => 'Category created successfully.'
+        'error'   => 'Company was not created, please try again.',
+        'success' => 'Company created successfully.'
     ),
-
     'update' => array(
-        'error'   => 'Category was not updated, please try again',
-        'success' => 'Category updated successfully.'
+        'error'   => 'Company was not updated, please try again',
+        'success' => 'Company updated successfully.'
     ),
-
     'delete' => array(
-        'confirm'   => 'Are you sure you wish to delete this category?',
-        'error'   => 'There was an issue deleting the category. Please try again.',
-        'success' => 'The category was deleted successfully.'
+        'confirm' => 'Are you sure you wish to delete this company?',
+        'error'   => 'There was an issue deleting the company. Please try again.',
+        'success' => 'The Company was deleted successfully.'
     )
-
 );
