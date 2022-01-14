1f311e28932637e25ca7f105d1d165fdcbf9e79d
Brady Wetherington <uberbrady@gmail.com>
2022-01-14 00:24:42 -0800

    New translations general.php (Afrikaans) Fixed: New translations general.php from Crowdin

--- resources/lang/af-ZA/general.php
+++ resources/lang/af-ZA/general.php
@@ -1,12 +1,15 @@
 <?php
 
 return array(
-    'about_asset_depreciations'  			=> 'About Asset Depreciations',
-    'about_depreciations'  					=> 'You can set up asset depreciations to depreciate assets based on straight-line depreciation.',
-    'asset_depreciations'  					=> 'Asset Depreciations',
-    'create'  					            => 'Create Depreciation',
-    'depreciation_name'  					=> 'Depreciation Name',
-    'number_of_months'  					=> 'Number of Months',
-    'update'  					            => 'Update Depreciation',
-
+    'component_name'                  => 'Component Name',
+    'checkin'                             => 'Checkin Component',
+    'checkout'                             => 'Checkout Component',
+    'cost'				=> 'Purchase Cost',
+    'create'                             => 'Create Component',
+    'edit'                             => 'Edit Component',
+    'date'					=> 'Purchase Date',
+    'order'					=> 'Order Number',
+    'remaining' 			             => 'Remaining',
+    'total' 			                 => 'Total',
+    'update'                            => 'Update Component',
 );
