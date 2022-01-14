f6f3dc9c07eb492bf8e518e94216e96291e9d2af
Brady Wetherington <uberbrady@gmail.com>
2022-01-14 00:30:53 -0800

    New translations table.php (Greek) Fixed: New translations table.php from Crowdin

--- resources/lang/el-GR/table.php
+++ resources/lang/el-GR/table.php
@@ -2,9 +2,9 @@
 
 return array(
 
-    'actions'	 	=> 'Actions',
-    'action' 		=> 'Action',
-    'by'      		=> 'By',
-    'item' 			=> 'Item',
+    'id'      => 'ID',
+    'months'   => 'Months',
+    'term'   => 'Term',
+    'title'      => 'Name ',
 
 );
