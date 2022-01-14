648f049262394234c2b647e78234225be58bbbb2
Brady Wetherington <uberbrady@gmail.com>
2022-01-14 00:28:07 -0800

    New translations message.php (Czech) Fixed: New translations message.php from Crowdin

--- resources/lang/cs-CZ/message.php
+++ resources/lang/cs-CZ/message.php
@@ -2,21 +2,35 @@
 
 return array(
 
-    'group_exists'        => 'Group already exists!',
-    'group_not_found'     => 'Group [:id] does not exist.',
-    'group_name_required' => 'The name field is required',
+    'does_not_exist' => 'Consumable does not exist.',
 
-    'success' => array(
-        'create' => 'Group was successfully created.',
-        'update' => 'Group was successfully updated.',
-        'delete' => 'Group was successfully deleted.',
+    'create' => array(
+        'error'   => 'Consumable was not created, please try again.',
+        'success' => 'Consumable created successfully.'
+    ),
+
+    'update' => array(
+        'error'   => 'Consumable was not updated, please try again',
+        'success' => 'Consumable updated successfully.'
     ),
 
     'delete' => array(
-        'confirm'   => 'Are you sure you wish to delete this group?',
-        'create' => 'There was an issue creating the group. Please try again.',
-        'update' => 'There was an issue updating the group. Please try again.',
-        'delete' => 'There was an issue deleting the group. Please try again.',
+        'confirm'   => 'Are you sure you wish to delete this consumable?',
+        'error'   => 'There was an issue deleting the consumable. Please try again.',
+        'success' => 'The consumable was deleted successfully.'
     ),
+
+     'checkout' => array(
+        'error'   		=> 'Consumable was not checked out, please try again',
+        'success' 		=> 'Consumable checked out successfully.',
+        'user_does_not_exist' => 'That user is invalid. Please try again.'
+    ),
+
+    'checkin' => array(
+        'error'   		=> 'Consumable was not checked in, please try again',
+        'success' 		=> 'Consumable checked in successfully.',
+        'user_does_not_exist' => 'That user is invalid. Please try again.'
+    )
+
 
 );
