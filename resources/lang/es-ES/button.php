d4fb774bf814934053570e1cbbf98ceadd24ef69
Brady Wetherington <uberbrady@gmail.com>
2022-01-13 22:10:41 -0800

    New translations button.php (Spanish) Fixed: New translations button.php from Crowdin

--- resources/lang/es-ES/button.php
+++ resources/lang/es-ES/button.php
@@ -14,4 +14,5 @@ return array(
 	'select_file'				=> 'Seleccionar Archivo...',
     'select_files'				=> 'Selecciona Archivos...',
     'generate_labels'           => '{1} Generate Label|[2,*] Generate Labels',
+    'send_password_link'       => 'Send Password Reset Link',
 );
