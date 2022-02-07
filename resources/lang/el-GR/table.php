e39add96dcfdc0f7cf3c1d51f89d71fc8349dcf2
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 12:11:28 -0800

    New translations table.php (Greek) Fixed: New translations table.php from Crowdin

--- resources/lang/el-GR/table.php
+++ resources/lang/el-GR/table.php
@@ -2,16 +2,10 @@
 
 return array(
 
-    'create'				=> 'Create Asset Model',
-    'created_at' 			=> 'Created at',
-    'eol'	 				=> 'EOL',
-    'modelnumber'   		=> 'Model No.',
-    'name'      			=> 'Asset Model Name',
-    'numassets' 			=> 'Assets',
-    'title'					=> 'Asset Models',
-    'update'				=> 'Update Asset Model',
-    'view'					=> 'View Asset Model',
-    'update'				=> 'Update Asset Model',
-    'clone'				=> 'Clone Model',
-    'edit'				=> 'Edit Model',
-);
+    'id'                        => 'ID',
+    'name'                      => 'Department Name',
+    'manager'                   => 'Manager',
+    'location'                  => 'Location',
+    'create'                    => 'Create Department',
+    'update'                    => 'Update Department',
+    );
