b472b9df3f881713b61233fb02184feaea15df08
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 12:08:17 -0800

    New translations general.php (Czech) Fixed: New translations general.php from Crowdin

--- resources/lang/cs-CZ/general.php
+++ resources/lang/cs-CZ/general.php
@@ -1,13 +1,4 @@
 <?php
-
-return array(
-    'about_asset_depreciations'  			=> 'About Asset Depreciations',
-    'about_depreciations'  					=> 'You can set up asset depreciations to depreciate assets based on straight-line depreciation.',
-    'asset_depreciations'  					=> 'Asset Depreciations',
-    'create'  					            => 'Create Depreciation',
-    'depreciation_name'  					=> 'Depreciation Name',
-    'number_of_months'  					=> 'Number of Months',
-    'update'  					            => 'Update Depreciation',
-    'depreciation_min'                      => 'Minimum Value after Depreciation'
-
-);
+return [
+    'select_company' => 'Select Company',
+];
