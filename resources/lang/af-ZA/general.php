c3ca4d0d3923c6abd26c45dd43fc242e33c4574a
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 12:05:17 -0800

    New translations general.php (Afrikaans) Fixed: New translations general.php from Crowdin

--- resources/lang/af-ZA/general.php
+++ resources/lang/af-ZA/general.php
@@ -1,4 +1,13 @@
 <?php
-return [
-    'select_company' => 'Select Company',
-];
+
+return array(
+    'about_asset_depreciations'  			=> 'About Asset Depreciations',
+    'about_depreciations'  					=> 'You can set up asset depreciations to depreciate assets based on straight-line depreciation.',
+    'asset_depreciations'  					=> 'Asset Depreciations',
+    'create'  					            => 'Create Depreciation',
+    'depreciation_name'  					=> 'Depreciation Name',
+    'number_of_months'  					=> 'Number of Months',
+    'update'  					            => 'Update Depreciation',
+    'depreciation_min'                      => 'Minimum Value after Depreciation'
+
+);
