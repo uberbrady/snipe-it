2cbcc0d255fcc4a3b2dd5276a9494eb08d600a14
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 11:53:24 -0800

    New translations message.php (Turkish) Fixed: New translations message.php from Crowdin

--- resources/lang/tr-TR/message.php
+++ resources/lang/tr-TR/message.php
@@ -2,26 +2,56 @@
 
 return array(
 
-    'does_not_exist' => 'Location does not exist.',
-    'assoc_users'	 => 'This location is currently associated with at least one user and cannot be deleted. Please update your users to no longer reference this location and try again. ',
-    'assoc_assets'	 => 'This location is currently associated with at least one asset and cannot be deleted. Please update your assets to no longer reference this location and try again. ',
-    'assoc_child_loc'	 => 'This location is currently the parent of at least one child location and cannot be deleted. Please update your locations to no longer reference this location and try again. ',
+    'field' => array(
+        'invalid'   => 'That field does not exist.',
+        'already_added'   => 'Field already added',
 
+        'create' => array(
+            'error'   => 'Field was not created, please try again.',
+            'success' => 'Field created successfully.',
+            'assoc_success' => 'Field successfully added to fieldset.'
+        ),
 
-    'create' => array(
-        'error'   => 'Location was not created, please try again.',
-        'success' => 'Location created successfully.'
+        'update' => array(
+            'error'   => 'Field was not updated, please try again',
+            'success' => 'Field updated successfully.'
+        ),
+
+        'delete' => array(
+            'confirm'   	=> 'Are you sure you wish to delete this field?',
+            'error'   => 'There was an issue deleting the field. Please try again.',
+            'success' => 'The field was deleted successfully.',
+            'in_use'   => 'Field is still in use.',
+        )
+
     ),
 
-    'update' => array(
-        'error'   => 'Location was not updated, please try again',
-        'success' => 'Location updated successfully.'
+    'fieldset' => array(
+
+        'does_not_exist' => 'Fieldset does not exist',
+
+        'create' => array(
+            'error'   => 'Fieldset was not created, please try again.',
+            'success' => 'Fieldset created successfully.'
+        ),
+
+        'update' => array(
+            'error'   => 'Fieldset was not updated, please try again',
+            'success' => 'Fieldset updated successfully.'
+        ),
+
+        'delete' => array(
+            'confirm'   	=> 'Are you sure you wish to delete this fieldset?',
+            'error'   => 'There was an issue deleting the fieldset. Please try again.',
+            'success' => 'The fieldset was deleted successfully.',
+            'in_use'   => 'Fieldset is still in use.',
+        )
+
     ),
 
-    'delete' => array(
-        'confirm'   	=> 'Are you sure you wish to delete this location?',
-        'error'   => 'There was an issue deleting the location. Please try again.',
-        'success' => 'The location was deleted successfully.'
-    )
+
+
+
+
 
 );
