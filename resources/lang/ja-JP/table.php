a3ae7e225f24e57542777a3775fe4023535cd83f
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 11:43:38 -0800

    New translations table.php (Japanese) Fixed: New translations table.php from Crowdin

--- resources/lang/ja-JP/table.php
+++ resources/lang/ja-JP/table.php
@@ -1,10 +1,10 @@
 <?php
 
 return array(
-
-    'actions'	 	=> 'Actions',
-    'action' 		=> 'Action',
-    'by'      		=> 'By',
-    'item' 			=> 'Item',
+	'eula_text'      			=> 'EULA',
+    'id'      					=> 'ID',
+    'parent'   					=> 'Parent',
+    'require_acceptance'      	=> 'Acceptance',
+    'title'      				=> 'Asset Category Name',
 
 );
