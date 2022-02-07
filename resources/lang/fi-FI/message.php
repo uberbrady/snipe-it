8aec87ac6f90c8287f241b1c9d1688e1ae246302
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 12:12:15 -0800

    New translations message.php (Finnish) Fixed: New translations message.php from Crowdin

--- resources/lang/fi-FI/message.php
+++ resources/lang/fi-FI/message.php
@@ -1,22 +1,21 @@
 <?php
 
-return array(
-
-    'group_exists'        => 'Group already exists!',
-    'group_not_found'     => 'Group [:id] does not exist.',
-    'group_name_required' => 'The name field is required',
-
-    'success' => array(
-        'create' => 'Group was successfully created.',
-        'update' => 'Group was successfully updated.',
-        'delete' => 'Group was successfully deleted.',
-    ),
-
-    'delete' => array(
-        'confirm'   => 'Are you sure you wish to delete this group?',
-        'create' => 'There was an issue creating the group. Please try again.',
-        'update' => 'There was an issue updating the group. Please try again.',
-        'delete' => 'There was an issue deleting the group. Please try again.',
-    ),
-
-);
+    return [
+        'not_found'                    => 'Asset Maintenance you were looking for was not found!',
+        'delete'                       => [
+            'confirm' => 'Are you sure you wish to delete this asset maintenance?',
+            'error'   => 'There was an issue deleting the asset maintenance. Please try again.',
+            'success' => 'The asset maintenance was deleted successfully.'
+        ],
+        'create'                       => [
+            'error'   => 'Asset Maintenance was not created, please try again.',
+            'success' => 'Asset Maintenance created successfully.'
+        ],
+        'edit'                       => [
+            'error'   => 'Asset Maintenance was not edited, please try again.',
+            'success' => 'Asset Maintenance edited successfully.'
+        ],
+        'asset_maintenance_incomplete' => 'Not Completed Yet',
+        'warranty'                     => 'Warranty',
+        'not_warranty'                 => 'Not Warranty',
+    ];
