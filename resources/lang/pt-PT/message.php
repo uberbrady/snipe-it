cd6622e474d00b2f13a41cd697504002546cb3a2
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 11:49:44 -0800

    New translations message.php (Portuguese) Fixed: New translations message.php from Crowdin

--- resources/lang/pt-PT/message.php
+++ resources/lang/pt-PT/message.php
@@ -2,26 +2,82 @@
 
 return array(
 
-    'does_not_exist' => 'Supplier does not exist.',
-
+    'undeployable' 		=> '<strong>Warning: </strong> This asset has been marked as currently undeployable.
+                        If this status has changed, please update the asset status.',
+    'does_not_exist' 	=> 'Asset does not exist.',
+    'does_not_exist_or_not_requestable' => 'Nice try. That asset does not exist or is not requestable.',
+    'assoc_users'	 	=> 'This asset is currently checked out to a user and cannot be deleted. Please check the asset in first, and then try deleting again. ',
 
     'create' => array(
-        'error'   => 'Supplier was not created, please try again.',
-        'success' => 'Supplier created successfully.'
+        'error'   		=> 'Asset was not created, please try again. :(',
+        'success' 		=> 'Asset created successfully. :)'
     ),
 
     'update' => array(
-        'error'   => 'Supplier was not updated, please try again',
-        'success' => 'Supplier updated successfully.'
+        'error'   			=> 'Asset was not updated, please try again',
+        'success' 			=> 'Asset updated successfully.',
+        'nothing_updated'	=>  'No fields were selected, so nothing was updated.',
     ),
 
+    'restore' => array(
+        'error'   		=> 'Asset was not restored, please try again',
+        'success' 		=> 'Asset restored successfully.'
+    ),
+
+    'audit' => array(
+        'error'   		=> 'Asset audit was unsuccessful. Please try again.',
+        'success' 		=> 'Asset audit successfully logged.'
+    ),
+
+
+    'deletefile' => array(
+        'error'   => 'File not deleted. Please try again.',
+        'success' => 'File successfully deleted.',
+    ),
+
+    'upload' => array(
+        'error'   => 'File(s) not uploaded. Please try again.',
+        'success' => 'File(s) successfully uploaded.',
+        'nofiles' => 'You did not select any files for upload, or the file you are trying to upload is too large',
+        'invalidfiles' => 'One or more of your files is too large or is a filetype that is not allowed. Allowed filetypes are png, gif, jpg, doc, docx, pdf, and txt.',
+    ),
+
+    'import' => array(
+        'error'                 => 'Some items did not import correctly.',
+        'errorDetail'           => 'The following Items were not imported because of errors.',
+        'success'               => "Your file has been imported",
+        'file_delete_success'   => "Your file has been been successfully deleted",
+        'file_delete_error'      => "The file was unable to be deleted",
+    ),
+
+
     'delete' => array(
-        'confirm'   => 'Are you sure you wish to delete this supplier?',
-        'error'   => 'There was an issue deleting the supplier. Please try again.',
-        'success' => 'Supplier was deleted successfully.',
-        'assoc_assets'	 => 'This supplier is currently associated with :asset_count asset(s) and cannot be deleted. Please update your assets to no longer reference this supplier and try again. ',
-        'assoc_licenses'	 => 'This supplier is currently associated with :licenses_count licences(s) and cannot be deleted. Please update your licenses to no longer reference this supplier and try again. ',
-        'assoc_maintenances'	 => 'This supplier is currently associated with :asset_maintenances_count asset maintenances(s) and cannot be deleted. Please update your asset maintenances to no longer reference this supplier and try again. ',
+        'confirm'   	=> 'Are you sure you wish to delete this asset?',
+        'error'   		=> 'There was an issue deleting the asset. Please try again.',
+        'nothing_updated'   => 'No assets were selected, so nothing was deleted.',
+        'success' 		=> 'The asset was deleted successfully.'
+    ),
+
+    'checkout' => array(
+        'error'   		=> 'Asset was not checked out, please try again',
+        'success' 		=> 'Asset checked out successfully.',
+        'user_does_not_exist' => 'That user is invalid. Please try again.',
+        'not_available' => 'That asset is not available for checkout!',
+        'no_assets_selected' => 'You must select at least one asset from the list'
+    ),
+
+    'checkin' => array(
+        'error'   		=> 'Asset was not checked in, please try again',
+        'success' 		=> 'Asset checked in successfully.',
+        'user_does_not_exist' => 'That user is invalid. Please try again.',
+        'already_checked_in'  => 'That asset is already checked in.',
+
+    ),
+
+    'requests' => array(
+        'error'   		=> 'Asset was not requested, please try again',
+        'success' 		=> 'Asset requested successfully.',
+        'canceled'      => 'Checkout request successfully canceled'
     )
 
 );
