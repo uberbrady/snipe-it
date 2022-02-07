9113d964c4fbff74da7ab3f6859606b14e74bcaa
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 12:17:32 -0800

    New translations table.php (Korean) Fixed: New translations table.php from Crowdin

--- resources/lang/ko-KR/table.php
+++ resources/lang/ko-KR/table.php
@@ -2,8 +2,16 @@
 
 return array(
 
-    'id'         => 'Id',
-    'name'       => 'Name',
-    'users'      => '# of Users',
+    'assigned_to'   	=> 'Assigned To',
+    'checkout'   		=> 'In/Out',
+    'id'      			=> 'ID',
+    'license_email'   	=> 'License Email',
+    'license_name'   	=> 'Licensed To',
+    'purchase_date'   	=> 'Purchase Date',
+    'purchased'   		=> 'Purchased',
+    'seats'   			=> 'Seats',
+    'hardware'   		=> 'Hardware',
+    'serial'   			=> 'Serial',
+    'title'      		=> 'License',
 
 );
