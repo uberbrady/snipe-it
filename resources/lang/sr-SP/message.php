d5bcf97d4a721ffe4e15f6d9294ee4045f6b0e94
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 12:23:45 -0800

    New translations message.php (Serbian (Cyrillic)) Fixed: New translations message.php from Crowdin

--- resources/lang/sr-SP/message.php
+++ resources/lang/sr-SP/message.php
@@ -2,28 +2,56 @@
 
 return array(
 
-    'does_not_exist' => 'Manufacturer does not exist.',
-    'assoc_users'	 => 'This manufacturer is currently associated with at least one model and cannot be deleted. Please update your models to no longer reference this manufacturer and try again. ',
+    'field' => array(
+        'invalid'   => 'That field does not exist.',
+        'already_added'   => 'Field already added',
 
-    'create' => array(
-        'error'   => 'Manufacturer was not created, please try again.',
-        'success' => 'Manufacturer created successfully.'
+        'create' => array(
+            'error'   => 'Field was not created, please try again.',
+            'success' => 'Field created successfully.',
+            'assoc_success' => 'Field successfully added to fieldset.'
+        ),
+
+        'update' => array(
+            'error'   => 'Field was not updated, please try again',
+            'success' => 'Field updated successfully.'
+        ),
+
+        'delete' => array(
+            'confirm'   	=> 'Are you sure you wish to delete this field?',
+            'error'   => 'There was an issue deleting the field. Please try again.',
+            'success' => 'The field was deleted successfully.',
+            'in_use'   => 'Field is still in use.',
+        )
+
     ),
 
-    'update' => array(
-        'error'   => 'Manufacturer was not updated, please try again',
-        'success' => 'Manufacturer updated successfully.'
+    'fieldset' => array(
+
+        'does_not_exist' => 'Fieldset does not exist',
+
+        'create' => array(
+            'error'   => 'Fieldset was not created, please try again.',
+            'success' => 'Fieldset created successfully.'
+        ),
+
+        'update' => array(
+            'error'   => 'Fieldset was not updated, please try again',
+            'success' => 'Fieldset updated successfully.'
+        ),
+
+        'delete' => array(
+            'confirm'   	=> 'Are you sure you wish to delete this fieldset?',
+            'error'   => 'There was an issue deleting the fieldset. Please try again.',
+            'success' => 'The fieldset was deleted successfully.',
+            'in_use'   => 'Fieldset is still in use.',
+        )
+
     ),
 
-    'restore' => array(
-        'error'   => 'Manufacturer was not restored, please try again',
-        'success' => 'Manufacturer restored successfully.'
-    ),
 
-    'delete' => array(
-        'confirm'   => 'Are you sure you wish to delete this manufacturer?',
-        'error'   => 'There was an issue deleting the manufacturer. Please try again.',
-        'success' => 'The Manufacturer was deleted successfully.'
-    )
+
+
+
 
 );
