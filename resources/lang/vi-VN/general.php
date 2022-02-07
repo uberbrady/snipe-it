06a7561e249775d535b64eb7bfd21530160254fe
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 12:30:31 -0800

    New translations general.php (Vietnamese) Fixed: New translations general.php from Crowdin

--- resources/lang/vi-VN/general.php
+++ resources/lang/vi-VN/general.php
@@ -1,4 +1,14 @@
 <?php
-return [
-    'select_company' => 'Select Company',
-];
+
+    return [
+        'asset_maintenances' => 'Asset Maintenances',
+        'edit'               => 'Edit Asset Maintenance',
+        'delete'             => 'Delete Asset Maintenance',
+        'view'               => 'View Asset Maintenance Details',
+        'repair'             => 'Repair',
+        'maintenance'        => 'Maintenance',
+        'upgrade'            => 'Upgrade',
+        'calibration'        => 'Calibration',
+        'software_support'   => 'Software Support',
+        'hardware_support'   => 'Hardware Support',
+    ];
