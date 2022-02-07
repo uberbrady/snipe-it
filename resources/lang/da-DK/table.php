9e83da2a4b0eb53e1ffa84b994f23734a760f365
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 11:37:45 -0800

    New translations table.php (Danish) Fixed: New translations table.php from Crowdin

--- resources/lang/da-DK/table.php
+++ resources/lang/da-DK/table.php
@@ -1,17 +1,19 @@
 <?php
 
 return array(
-
-    'create'				=> 'Create Asset Model',
-    'created_at' 			=> 'Created at',
-    'eol'	 				=> 'EOL',
-    'modelnumber'   		=> 'Model No.',
-    'name'      			=> 'Asset Model Name',
-    'numassets' 			=> 'Assets',
-    'title'					=> 'Asset Models',
-    'update'				=> 'Update Asset Model',
-    'view'					=> 'View Asset Model',
-    'update'				=> 'Update Asset Model',
-    'clone'				=> 'Clone Model',
-    'edit'				=> 'Edit Model',
+    'about'      	=> 'About Status Labels',
+    'archived'      	=> 'Archived',
+    'create'      	=> 'Create Status Label',
+    'color'      	=> 'Chart Color',
+    'default_label' => 'Default Label',
+    'default_label_help' => 'This is used to ensure your most commonly used status labels appear at the top of the select box when creating/editing assets.',
+    'deployable'      	=> 'Deployable',
+    'info'      	=> 'Status labels are used to describe the various states your assets could be in. They may be out for repair, lost/stolen, etc. You can create new status labels for deployable, pending and archived assets.',
+    'name'      	=> 'Status Name',
+    'pending'      	=> 'Pending',
+    'status_type'   => 'Status Type',
+    'show_in_nav'   => 'Show in side nav',
+    'title'      	=> 'Status Labels',
+    'undeployable'  => 'Undeployable',
+    'update'      	=> 'Update Status Label',
 );
