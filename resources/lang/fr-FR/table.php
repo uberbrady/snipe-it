2ae640028f009f507dbbfd6c196753d7291bbed7
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 11:29:50 -0800

    New translations table.php (French) Fixed: New translations table.php from Crowdin

--- resources/lang/fr-FR/table.php
+++ resources/lang/fr-FR/table.php
@@ -1,11 +1,5 @@
 <?php
 
 return array(
-
-    'id'                        => 'ID',
-    'name'                      => 'Department Name',
-    'manager'                   => 'Manager',
-    'location'                  => 'Location',
-    'create'                    => 'Create Department',
-    'update'                    => 'Update Department',
-    );
+    'title'      				=> 'Component Name',
+);
