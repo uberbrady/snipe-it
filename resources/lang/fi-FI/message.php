ed2065074fee60dacac92d4e0df51158f3ee77a0
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 12:12:05 -0800

    New translations message.php (Finnish) Fixed: New translations message.php from Crowdin

--- resources/lang/fi-FI/message.php
+++ resources/lang/fi-FI/message.php
@@ -2,35 +2,56 @@
 
 return array(
 
-    'does_not_exist' => 'Component does not exist.',
+    'field' => array(
+        'invalid'   => 'That field does not exist.',
+        'already_added'   => 'Field already added',
 
-    'create' => array(
-        'error'   => 'Component was not created, please try again.',
-        'success' => 'Component created successfully.'
+        'create' => array(
+            'error'   => 'Field was not created, please try again.',
+            'success' => 'Field created successfully.',
+            'assoc_success' => 'Field successfully added to fieldset.'
+        ),
+
+        'update' => array(
+            'error'   => 'Field was not updated, please try again',
+            'success' => 'Field updated successfully.'
+        ),
+
+        'delete' => array(
+            'confirm'   	=> 'Are you sure you wish to delete this field?',
+            'error'   => 'There was an issue deleting the field. Please try again.',
+            'success' => 'The field was deleted successfully.',
+            'in_use'   => 'Field is still in use.',
+        )
+
     ),
 
-    'update' => array(
-        'error'   => 'Component was not updated, please try again',
-        'success' => 'Component updated successfully.'
+    'fieldset' => array(
+
+        'does_not_exist' => 'Fieldset does not exist',
+
+        'create' => array(
+            'error'   => 'Fieldset was not created, please try again.',
+            'success' => 'Fieldset created successfully.'
+        ),
+
+        'update' => array(
+            'error'   => 'Fieldset was not updated, please try again',
+            'success' => 'Fieldset updated successfully.'
+        ),
+
+        'delete' => array(
+            'confirm'   	=> 'Are you sure you wish to delete this fieldset?',
+            'error'   => 'There was an issue deleting the fieldset. Please try again.',
+            'success' => 'The fieldset was deleted successfully.',
+            'in_use'   => 'Fieldset is still in use.',
+        )
+
     ),
 
-    'delete' => array(
-        'confirm'   => 'Are you sure you wish to delete this component?',
-        'error'   => 'There was an issue deleting the component. Please try again.',
-        'success' => 'The component was deleted successfully.'
-    ),
 
-     'checkout' => array(
-        'error'   		=> 'Component was not checked out, please try again',
-        'success' 		=> 'Component checked out successfully.',
-        'user_does_not_exist' => 'That user is invalid. Please try again.'
-    ),
 
-    'checkin' => array(
-        'error'   		=> 'Component was not checked in, please try again',
-        'success' 		=> 'Component checked in successfully.',
-        'user_does_not_exist' => 'That user is invalid. Please try again.'
-    )
+
 
 
 );
