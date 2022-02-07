0e8314a39a06ce92edca2f2e6a27eb908e1aa1b2
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 11:47:50 -0800

    New translations table.php (Norwegian) Fixed: New translations table.php from Crowdin

--- resources/lang/no-NO/table.php
+++ resources/lang/no-NO/table.php
@@ -1,8 +1,10 @@
 <?php
 
-    return [
-        'title'         => 'Asset Maintenance',
-        'asset_name'    => 'Asset Name',
-        'is_warranty'   => 'Warranty',
-        'dl_csv'        => 'Download CSV'
-    ];
+return array(
+
+    'actions'	 	=> 'Actions',
+    'action' 		=> 'Action',
+    'by'      		=> 'By',
+    'item' 			=> 'Item',
+
+);
