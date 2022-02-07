6d21faa2a6723a8606aa1bb8953c6173d294ec76
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 12:19:34 -0800

    New translations message.php (Norwegian) Fixed: New translations message.php from Crowdin

--- resources/lang/no-NO/message.php
+++ resources/lang/no-NO/message.php
@@ -1,18 +1,22 @@
 <?php
+
 return array(
-    'does_not_exist' => 'Company does not exist.',
-    'assoc_users'    => 'This company is currently associated with at least one model and cannot be deleted. Please update your models to no longer reference this company and try again. ',
-    'create' => array(
-        'error'   => 'Company was not created, please try again.',
-        'success' => 'Company created successfully.'
-    ),
+
+
     'update' => array(
-        'error'   => 'Company was not updated, please try again',
-        'success' => 'Company updated successfully.'
+        'error'                 => 'An error has occurred while updating. ',
+        'success'               => 'Settings updated successfully.'
     ),
-    'delete' => array(
-        'confirm' => 'Are you sure you wish to delete this company?',
-        'error'   => 'There was an issue deleting the company. Please try again.',
-        'success' => 'The Company was deleted successfully.'
-    )
+    'backup' => array(
+        'delete_confirm'        => 'Are you sure you would like to delete this backup file? This action cannot be undone. ',
+        'file_deleted'          => 'The backup file was successfully deleted. ',
+        'generated'             => 'A new backup file was successfully created.',
+        'file_not_found'        => 'That backup file could not be found on the server.',
+    ),
+    'purge' => array(
+        'error'     => 'An error has occurred while purging. ',
+        'validation_failed'     => 'Your purge confirmation is incorrect. Please type the word "DELETE" in the confirmation box.',
+        'success'               => 'Deleted records successfully purged.'
+    ),
+
 );
