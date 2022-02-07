48f1f0656344b46fe60fc881c251717653eb8fb6
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 12:28:28 -0800

    New translations table.php (Chinese Simplified) Fixed: New translations table.php from Crowdin

--- resources/lang/zh-CN/table.php
+++ resources/lang/zh-CN/table.php
@@ -1,19 +1,17 @@
 <?php
 
 return array(
-    'about'      	=> 'About Status Labels',
-    'archived'      	=> 'Archived',
-    'create'      	=> 'Create Status Label',
-    'color'      	=> 'Chart Color',
-    'default_label' => 'Default Label',
-    'default_label_help' => 'This is used to ensure your most commonly used status labels appear at the top of the select box when creating/editing assets.',
-    'deployable'      	=> 'Deployable',
-    'info'      	=> 'Status labels are used to describe the various states your assets could be in. They may be out for repair, lost/stolen, etc. You can create new status labels for deployable, pending and archived assets.',
-    'name'      	=> 'Status Name',
-    'pending'      	=> 'Pending',
-    'status_type'   => 'Status Type',
-    'show_in_nav'   => 'Show in side nav',
-    'title'      	=> 'Status Labels',
-    'undeployable'  => 'Undeployable',
-    'update'      	=> 'Update Status Label',
+
+    'assigned_to'   	=> 'Assigned To',
+    'checkout'   		=> 'In/Out',
+    'id'      			=> 'ID',
+    'license_email'   	=> 'License Email',
+    'license_name'   	=> 'Licensed To',
+    'purchase_date'   	=> 'Purchase Date',
+    'purchased'   		=> 'Purchased',
+    'seats'   			=> 'Seats',
+    'hardware'   		=> 'Hardware',
+    'serial'   			=> 'Serial',
+    'title'      		=> 'License',
+
 );
