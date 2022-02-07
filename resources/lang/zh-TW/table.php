72e25d82432dcb3d5808db66ee7fc1d70946e86e
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 11:57:06 -0800

    New translations table.php (Chinese Traditional) Fixed: New translations table.php from Crowdin

--- resources/lang/zh-TW/table.php
+++ resources/lang/zh-TW/table.php
@@ -1,8 +1,5 @@
 <?php
 
-    return [
-        'title'         => 'Asset Maintenance',
-        'asset_name'    => 'Asset Name',
-        'is_warranty'   => 'Warranty',
-        'dl_csv'        => 'Download CSV'
-    ];
+return array(
+    'title'      				=> 'Consumable Name',
+);
