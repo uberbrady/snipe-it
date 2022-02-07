3f451c907c8b102f4ed06ee36489bb64317202a2
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 11:38:04 -0800

    New translations general.php (German) Fixed: New translations general.php from Crowdin

--- resources/lang/de-DE/general.php
+++ resources/lang/de-DE/general.php
@@ -1,20 +1,16 @@
 <?php
 
-return array(
-    'accessory_category' 				=> 'Accessory Category',
-    'accessory_name'  					=> 'Accessory Name',
-    'checkout'  							=> 'Checkout Accessory',
-    'checkin'  							=> 'Checkin Accessory',
-    'create'  							=> 'Create Accessory',
-    'edit'  							=> 'Edit Accessory',
-    'eula_text'							=> 'Category EULA',
-    'eula_text_help'					=> 'This field allows you to customize your EULAs for specific types of assets. If you only have one EULA for all of your assets, you can check the box below to use the primary default.',
-    'require_acceptance'				=> 'Require users to confirm acceptance of assets in this category.',
-    'no_default_eula'					=> 'No primary default EULA found. Add one in Settings.',
-    'total'  							=> 'Total',
-    'remaining'  						=> 'Avail',
-    'update'  							=> 'Update Accessory',
-    'use_default_eula'					=> 'Use the <a href="#" data-toggle="modal" data-target="#eulaModal">primary default EULA</a> instead.',
-    'use_default_eula_disabled'			=> '<del>Use the primary default EULA instead.</del> No primary default EULA is set. Please add one in Settings.',
+return [
+    'send_password_link'	        => 'Send Password Reset Link',
+    'email_reset_password'			=> 'Email Password Reset',
+    'reset_password'			    => 'Reset Password',
+    'saml_login'                    => 'Login via SAML',
+    'login'                         => 'Login',
+    'login_prompt'                  => 'Please Login',
+    'forgot_password'               => 'I forgot my password',
+    'ldap_reset_password'           => 'Please click here to reset your LDAP password',
+    'remember_me'                   => 'Remember Me',
+    'username_help_top'                 => 'Enter your <strong>username</strong> to be emailed a password reset link.',
+    'username_help_bottom'          => 'Your username and email address <em>may</em> be the same, but may not be, depending on your configuration. If you cannot remember your username, contact your administrator. <br><br><strong>Usernames without an associated email address will not be emailed a password reset link.</strong> ',
+    ];
 
-);
