34c00ac401c191226fbe16ca04421778c06898ee
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 12:32:11 -0800

    New translations general.php (Portuguese, Brazilian) Fixed: New translations general.php from Crowdin

--- resources/lang/pt-BR/general.php
+++ resources/lang/pt-BR/general.php
@@ -1,22 +1,13 @@
 <?php
 
 return array(
-    'about_assets_title'           => 'About Assets',
-    'about_assets_text'            => 'Assets are items tracked by serial number or asset tag.  They tend to be higher value items where identifying a specific item matters.',
-	'archived'  				=> 'Archived',
-    'asset'  					=> 'Asset',
-    'bulk_checkout'             => 'Checkout Assets',
-    'checkin'  					=> 'Checkin Asset',
-    'checkout'  				=> 'Checkout Asset',
-    'clone'  					=> 'Clone Asset',
-    'deployable'  				=> 'Deployable',
-    'deleted'  					=> 'This asset has been deleted. <a href="/hardware/:asset_id/restore">Click here to restore it</a>.',
-    'edit'  					=> 'Edit Asset',
-    'model_deleted'  			=> 'This Assets model has been deleted. You must restore the model before you can restore the Asset.<br/> <a href="/hardware/models/:model_id/restore">Click here to restore the model</a>.',
-    'requestable'               => 'Requestable',
-    'requested'				    => 'Requested',
-    'restore'  					=> 'Restore Asset',
-    'pending'  					=> 'Pending',
-    'undeployable'  			=> 'Undeployable',
-    'view'  					=> 'View Asset',
+    'about_asset_depreciations'  			=> 'About Asset Depreciations',
+    'about_depreciations'  					=> 'You can set up asset depreciations to depreciate assets based on straight-line depreciation.',
+    'asset_depreciations'  					=> 'Asset Depreciations',
+    'create'  					            => 'Create Depreciation',
+    'depreciation_name'  					=> 'Depreciation Name',
+    'number_of_months'  					=> 'Number of Months',
+    'update'  					            => 'Update Depreciation',
+    'depreciation_min'                      => 'Minimum Value after Depreciation'
+
 );
