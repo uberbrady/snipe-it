42c4ca0e210e817aa08e45eda36118a967593469
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 11:37:16 -0800

    New translations general.php (German) Fixed: New translations general.php from Crowdin

--- resources/lang/de-DE/general.php
+++ resources/lang/de-DE/general.php
@@ -1,21 +1,14 @@
 <?php
 
-return array(
-    'about_licenses_title'            => 'About Licenses',
-    'about_licenses'                  => 'Licenses are used to track software.  They have a specified number of seats that can be checked out to individuals',
-    'checkin'  					=> 'Checkin License Seat',
-    'checkout_history'  		=> 'Checkout History',
-    'checkout'  				=> 'Checkout License Seat',
-    'edit'  					=> 'Edit License',
-    'filetype_info'				=> 'Allowed filetypes are png, gif, jpg, jpeg, doc, docx, pdf, txt, zip, and rar.',
-    'clone'  					=> 'Clone License',
-    'history_for'  				=> 'History for ',
-    'in_out'  					=> 'In/Out',
-    'info'  					=> 'License Info',
-    'license_seats'  			=> 'License Seats',
-    'seat'  					=> 'Seat',
-    'seats'  					=> 'Seats',
-    'software_licenses'  		=> 'Software Licenses',
-    'user'  					=> 'User',
-    'view'  					=> 'View License',
-);
+    return [
+        'asset_maintenances' => 'Asset Maintenances',
+        'edit'               => 'Edit Asset Maintenance',
+        'delete'             => 'Delete Asset Maintenance',
+        'view'               => 'View Asset Maintenance Details',
+        'repair'             => 'Repair',
+        'maintenance'        => 'Maintenance',
+        'upgrade'            => 'Upgrade',
+        'calibration'        => 'Calibration',
+        'software_support'   => 'Software Support',
+        'hardware_support'   => 'Hardware Support',
+    ];
