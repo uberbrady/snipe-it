f131bbf45379e00843d624cb163625356ca80de6
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 12:25:56 -0800

    New translations message.php (Turkish) Fixed: New translations message.php from Crowdin

--- resources/lang/tr-TR/message.php
+++ resources/lang/tr-TR/message.php
@@ -2,41 +2,21 @@
 
 return array(
 
-    'does_not_exist' => 'Model does not exist.',
-    'assoc_users'	 => 'This model is currently associated with one or more assets and cannot be deleted. Please delete the assets, and then try deleting again. ',
-
-
-    'create' => array(
-        'error'   => 'Model was not created, please try again.',
-        'success' => 'Model created successfully.',
-        'duplicate_set' => 'An asset model with that name, manufacturer and model number already exists.',
-    ),
 
     'update' => array(
-        'error'   => 'Model was not updated, please try again',
-        'success' => 'Model updated successfully.'
+        'error'                 => 'An error has occurred while updating. ',
+        'success'               => 'Settings updated successfully.'
     ),
-
-    'delete' => array(
-        'confirm'   => 'Are you sure you wish to delete this asset model?',
-        'error'   => 'There was an issue deleting the model. Please try again.',
-        'success' => 'The model was deleted successfully.'
+    'backup' => array(
+        'delete_confirm'        => 'Are you sure you would like to delete this backup file? This action cannot be undone. ',
+        'file_deleted'          => 'The backup file was successfully deleted. ',
+        'generated'             => 'A new backup file was successfully created.',
+        'file_not_found'        => 'That backup file could not be found on the server.',
     ),
-
-    'restore' => array(
-        'error'   		=> 'Model was not restored, please try again',
-        'success' 		=> 'Model restored successfully.'
-    ),
-
-    'bulkedit' => array(
-        'error'   		=> 'No fields were changed, so nothing was updated.',
-        'success' 		=> 'Models updated.'
-    ),
-
-    'bulkdelete' => array(
-        'error'   		    => 'No models were selected, so nothing was deleted.',
-        'success' 		    => ':success_count model(s) deleted!',
-        'success_partial' 	=> ':success_count model(s) were deleted, however :fail_count were unable to be deleted because they still have assets associated with them.'
+    'purge' => array(
+        'error'     => 'An error has occurred while purging. ',
+        'validation_failed'     => 'Your purge confirmation is incorrect. Please type the word "DELETE" in the confirmation box.',
+        'success'               => 'Deleted records successfully purged.'
     ),
 
 );
