c51e443c50f1658b9294f85283a6daa9cceb7450
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 12:26:22 -0800

    New translations general.php (Turkish) Fixed: New translations general.php from Crowdin

--- resources/lang/tr-TR/general.php
+++ resources/lang/tr-TR/general.php
@@ -1,17 +1,13 @@
 <?php
 
 return array(
-    'about_kits_title'                  => 'About Predefined Kits',
-    'about_kits_text'                   => 'Predefined Kits let you quickly check out a collection of items (assets, licenses, etc) to a user. This can be helpful when your onboarding process is consistent across many users and all users receive the same items.',
-    'checkout'                          => 'Checkout Kit ',
-    'create_success'                    => 'Kit was successfully created.',
-    'create'                            => 'Create Predefined Kit',
-    'update'                            => 'Update Predefined Kit',
-    'delete_success'                    => 'Kit was successfully deleted.',
-    'update_success'                    => 'Kit was successfully updated.',
-    'none_models'                       => 'There are not enough available assets for :model to checkout. :qty are required. ',
-    'none_licenses'                     => 'There are not enough available seats for :license to checkout. :qty are required. ',
-    'none_consumables'                  => 'There are not enough available units of :consumable to checkout. :qty are required. ',
-    'none_accessory'                    => 'There are not enough available units of :accessory to checkout. :qty are required. ',
+    'about_asset_depreciations'  			=> 'About Asset Depreciations',
+    'about_depreciations'  					=> 'You can set up asset depreciations to depreciate assets based on straight-line depreciation.',
+    'asset_depreciations'  					=> 'Asset Depreciations',
+    'create'  					            => 'Create Depreciation',
+    'depreciation_name'  					=> 'Depreciation Name',
+    'number_of_months'  					=> 'Number of Months',
+    'update'  					            => 'Update Depreciation',
+    'depreciation_min'                      => 'Minimum Value after Depreciation'
 
 );
