e7c99e4261adf22bc7b3fae3dddcb063d4063a60
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 11:45:26 -0800

    New translations message.php (Korean) Fixed: New translations message.php from Crowdin

--- resources/lang/ko-KR/message.php
+++ resources/lang/ko-KR/message.php
@@ -2,28 +2,35 @@
 
 return array(
 
-    'does_not_exist' => 'Manufacturer does not exist.',
-    'assoc_users'	 => 'This manufacturer is currently associated with at least one model and cannot be deleted. Please update your models to no longer reference this manufacturer and try again. ',
+    'does_not_exist' => 'Consumable does not exist.',
 
     'create' => array(
-        'error'   => 'Manufacturer was not created, please try again.',
-        'success' => 'Manufacturer created successfully.'
+        'error'   => 'Consumable was not created, please try again.',
+        'success' => 'Consumable created successfully.'
     ),
 
     'update' => array(
-        'error'   => 'Manufacturer was not updated, please try again',
-        'success' => 'Manufacturer updated successfully.'
-    ),
-
-    'restore' => array(
-        'error'   => 'Manufacturer was not restored, please try again',
-        'success' => 'Manufacturer restored successfully.'
+        'error'   => 'Consumable was not updated, please try again',
+        'success' => 'Consumable updated successfully.'
     ),
 
     'delete' => array(
-        'confirm'   => 'Are you sure you wish to delete this manufacturer?',
-        'error'   => 'There was an issue deleting the manufacturer. Please try again.',
-        'success' => 'The Manufacturer was deleted successfully.'
+        'confirm'   => 'Are you sure you wish to delete this consumable?',
+        'error'   => 'There was an issue deleting the consumable. Please try again.',
+        'success' => 'The consumable was deleted successfully.'
+    ),
+
+     'checkout' => array(
+        'error'   		=> 'Consumable was not checked out, please try again',
+        'success' 		=> 'Consumable checked out successfully.',
+        'user_does_not_exist' => 'That user is invalid. Please try again.'
+    ),
+
+    'checkin' => array(
+        'error'   		=> 'Consumable was not checked in, please try again',
+        'success' 		=> 'Consumable checked in successfully.',
+        'user_does_not_exist' => 'That user is invalid. Please try again.'
     )
+
 
 );
