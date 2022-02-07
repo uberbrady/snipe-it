1a48424a6646f643a427958812309e324a0ead63
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 11:42:25 -0800

    New translations table.php (Hungarian) Fixed: New translations table.php from Crowdin

--- resources/lang/hu-HU/table.php
+++ resources/lang/hu-HU/table.php
@@ -1,10 +1,5 @@
 <?php
 
 return array(
-
-    'id'      => 'ID',
-    'months'   => 'Months',
-    'term'   => 'Term',
-    'title'      => 'Name ',
-
+    'title'      				=> 'Component Name',
 );
