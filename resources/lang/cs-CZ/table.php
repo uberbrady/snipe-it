02dae12f1150e1bc3435e102a491e58fe10b58d5
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 12:08:20 -0800

    New translations table.php (Czech) Fixed: New translations table.php from Crowdin

--- resources/lang/cs-CZ/table.php
+++ resources/lang/cs-CZ/table.php
@@ -1,10 +1,11 @@
 <?php
 
 return array(
+	'dl_csv'      				=> 'Download CSV',
+	'eula_text'      			=> 'EULA',
+    'id'      					=> 'ID',
+    'require_acceptance'      	=> 'Acceptance',
+    'title'      				=> 'Accessory Name',
 
-    'id'      => 'ID',
-    'months'   => 'Months',
-    'term'   => 'Term',
-    'title'      => 'Name ',
 
 );
