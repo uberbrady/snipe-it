3cfd557b79d9f307dcac05bcef256d2c93335462
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 12:32:08 -0800

    New translations table.php (Portuguese, Brazilian) Fixed: New translations table.php from Crowdin

--- resources/lang/pt-BR/table.php
+++ resources/lang/pt-BR/table.php
@@ -1,8 +1,9 @@
 <?php
 
-    return [
-        'title'         => 'Asset Maintenance',
-        'asset_name'    => 'Asset Name',
-        'is_warranty'   => 'Warranty',
-        'dl_csv'        => 'Download CSV'
-    ];
+return array(
+
+    'id'         => 'Id',
+    'name'       => 'Name',
+    'users'      => '# of Users',
+
+);
