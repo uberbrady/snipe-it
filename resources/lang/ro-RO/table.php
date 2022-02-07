638a6911f3cf7f240b6ce3e04c6ce72f983c0e13
Brady Wetherington <uberbrady@gmail.com>
2022-02-07 12:01:56 -0800

    New translations table.php (Romanian) Fixed: New translations table.php from Crowdin

--- resources/lang/ro-RO/table.php
+++ resources/lang/ro-RO/table.php
@@ -1,9 +1,9 @@
 <?php
+
 return array(
-    'companies' => 'Companies',
-    'create'    => 'Create Company',
-    'title'     => 'Company',
-    'update'    => 'Update Company',
-    'name'      => 'Company Name',
-    'id'        => 'ID',
+
+    'id'         => 'Id',
+    'name'       => 'Name',
+    'users'      => '# of Users',
+
 );
