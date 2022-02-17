6c04f47f8f7053c1b9bfef62014be802636eacd6
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 20:18:52 -0700

    New translations general.php (Vietnamese) Fixed: New translations general.php from Crowdin

--- resources/lang/vi-VN/general.php
+++ resources/lang/vi-VN/general.php
@@ -1,15 +1,4 @@
 <?php
-
-return array(
-    'component_name'                  => 'Component Name',
-    'checkin'                             => 'Checkin Component',
-    'checkout'                             => 'Checkout Component',
-    'cost'				=> 'Purchase Cost',
-    'create'                             => 'Create Component',
-    'edit'                             => 'Edit Component',
-    'date'					=> 'Purchase Date',
-    'order'					=> 'Order Number',
-    'remaining' 			             => 'Remaining',
-    'total' 			                 => 'Total',
-    'update'                            => 'Update Component',
-);
+return [
+    'select_company' => 'Select Company',
+];
