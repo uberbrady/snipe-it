a8a52a078f1dccbb13c359b0b198902656bdce03
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 18:54:41 -0700

    New translations message.php (Hungarian) Fixed: New translations message.php from Crowdin

--- resources/lang/hu-HU/message.php
+++ resources/lang/hu-HU/message.php
@@ -2,44 +2,20 @@
 
 return array(
 
-    'account_already_exists' => 'An account with the this email already exists.',
-    'account_not_found'      => 'The username or password is incorrect.',
-    'account_not_activated'  => 'This user account is not activated.',
-    'account_suspended'      => 'This user account is suspended.',
-    'account_banned'         => 'This user account is banned.',
-    'throttle'               => 'Too many failed login attempts. Please try again in :minutes minutes.',
-
-    'two_factor' => array(
-        'already_enrolled'      => 'Your device is already enrolled.',
-        'success'               => 'You have successfully logged in.',
-        'code_required'         => 'Two-factor code is required.',
-        'invalid_code'          => 'Two-factor code is invalid.',
+    'does_not_exist' => 'Department does not exist.',
+    'assoc_users'	 => 'This department is currently associated with at least one user and cannot be deleted. Please update your users to no longer reference this department and try again. ',
+    'create' => array(
+        'error'   => 'Department was not created, please try again.',
+        'success' => 'Department created successfully.'
     ),
-
-    'signin' => array(
-        'error'   => 'There was a problem while trying to log you in, please try again.',
-        'success' => 'You have successfully logged in.',
+    'update' => array(
+        'error'   => 'Department was not updated, please try again',
+        'success' => 'Department updated successfully.'
     ),
-
-    'logout' => array(
-        'error'   => 'There was a problem while trying to log you out, please try again.',
-        'success' => 'You have successfully logged out.',
-    ),
-
-    'signup' => array(
-        'error'   => 'There was a problem while trying to create your account, please try again.',
-        'success' => 'Account sucessfully created.',
-    ),
-
-    'forgot-password' => array(
-        'error'   => 'There was a problem while trying to get a reset password code, please try again.',
-        'success' => 'If that email address exists in our system, a password recovery email has been sent.',
-    ),
-
-    'forgot-password-confirm' => array(
-        'error'   => 'There was a problem while trying to reset your password, please try again.',
-        'success' => 'Your password has been successfully reset.',
-    ),
-
+    'delete' => array(
+        'confirm'   	=> 'Are you sure you wish to delete this department?',
+        'error'   => 'There was an issue deleting the department. Please try again.',
+        'success' => 'The department was deleted successfully.'
+    )
 
 );
