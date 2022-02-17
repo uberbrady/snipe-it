f0813fe79be581517a8ad6efe104474b689a1491
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 18:55:37 -0700

    New translations message.php (Italian) Fixed: New translations message.php from Crowdin

--- resources/lang/it-IT/message.php
+++ resources/lang/it-IT/message.php
@@ -2,41 +2,36 @@
 
 return array(
 
-    'does_not_exist' => 'Model does not exist.',
-    'assoc_users'	 => 'This model is currently associated with one or more assets and cannot be deleted. Please delete the assets, and then try deleting again. ',
-
+    'does_not_exist' => 'The accessory [:id] does not exist.',
+    'assoc_users'	 => 'This accessory currently has :count items checked out to users. Please check in the accessories and and try again. ',
 
     'create' => array(
-        'error'   => 'Model was not created, please try again.',
-        'success' => 'Model created successfully.',
-        'duplicate_set' => 'An asset model with that name, manufacturer and model number already exists.',
+        'error'   => 'The accessory was not created, please try again.',
+        'success' => 'The accessory was successfully created.'
     ),
 
     'update' => array(
-        'error'   => 'Model was not updated, please try again',
-        'success' => 'Model updated successfully.'
+        'error'   => 'The accessory was not updated, please try again',
+        'success' => 'The accessory was updated successfully.'
     ),
 
     'delete' => array(
-        'confirm'   => 'Are you sure you wish to delete this asset model?',
-        'error'   => 'There was an issue deleting the model. Please try again.',
-        'success' => 'The model was deleted successfully.'
+        'confirm'   => 'Are you sure you wish to delete this accessory?',
+        'error'   => 'There was an issue deleting the accessory. Please try again.',
+        'success' => 'The accessory was deleted successfully.'
     ),
 
-    'restore' => array(
-        'error'   		=> 'Model was not restored, please try again',
-        'success' 		=> 'Model restored successfully.'
+     'checkout' => array(
+        'error'   		=> 'Accessory was not checked out, please try again',
+        'success' 		=> 'Accessory checked out successfully.',
+        'user_does_not_exist' => 'That user is invalid. Please try again.'
     ),
 
-    'bulkedit' => array(
-        'error'   		=> 'No fields were changed, so nothing was updated.',
-        'success' 		=> 'Models updated.'
-    ),
+    'checkin' => array(
+        'error'   		=> 'Accessory was not checked in, please try again',
+        'success' 		=> 'Accessory checked in successfully.',
+        'user_does_not_exist' => 'That user is invalid. Please try again.'
+    )
 
-    'bulkdelete' => array(
-        'error'   		    => 'No models were selected, so nothing was deleted.',
-        'success' 		    => ':success_count model(s) deleted!',
-        'success_partial' 	=> ':success_count model(s) were deleted, however :fail_count were unable to be deleted because they still have assets associated with them.'
-    ),
 
 );
