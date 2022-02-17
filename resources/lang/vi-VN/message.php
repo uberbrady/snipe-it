aa5798b6907404e6794d5c4bf3975cefde6e8809
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 20:19:18 -0700

    New translations message.php (Vietnamese) Fixed: New translations message.php from Crowdin

--- resources/lang/vi-VN/message.php
+++ resources/lang/vi-VN/message.php
@@ -2,31 +2,24 @@
 
 return array(
 
-    'does_not_exist' => 'Status Label does not exist.',
-    'assoc_assets'	 => 'This Status Label is currently associated with at least one Asset and cannot be deleted. Please update your assets to no longer reference this status and try again. ',
+    'does_not_exist' => 'Depreciation class does not exist.',
+    'assoc_users'	 => 'This depreciation is currently associated with one or more models and cannot be deleted. Please delete the models, and then try deleting again. ',
 
 
     'create' => array(
-        'error'   => 'Status Label was not created, please try again.',
-        'success' => 'Status Label created successfully.'
+        'error'   => 'Depreciation class was not created, please try again. :(',
+        'success' => 'Depreciation class created successfully. :)'
     ),
 
     'update' => array(
-        'error'   => 'Status Label was not updated, please try again',
-        'success' => 'Status Label updated successfully.'
+        'error'   => 'Depreciation class was not updated, please try again',
+        'success' => 'Depreciation class updated successfully.'
     ),
 
     'delete' => array(
-        'confirm'   => 'Are you sure you wish to delete this Status Label?',
-        'error'   => 'There was an issue deleting the Status Label. Please try again.',
-        'success' => 'The Status Label was deleted successfully.'
-    ),
-
-    'help' => array(
-        'undeployable'   => 'These assets cannot be assigned to anyone.',
-        'deployable'   => 'These assets can be checked out. Once they are assigned, they will assume a meta status of <i class="fa fa-circle text-blue"></i> <strong>Deployed</strong>.',
-        'archived'   => 'These assets cannot be checked out, and will only show up in the Archived view. This is useful for retaining information about assets for budgeting/historic purposes but keeping them out of the day-to-day asset list.',
-        'pending'   => 'These assets can not yet be assigned to anyone, often used for items that are out for repair, but are expected to return to circulation.',
-    ),
+        'confirm'   => 'Are you sure you wish to delete this depreciation class?',
+        'error'   => 'There was an issue deleting the depreciation class. Please try again.',
+        'success' => 'The depreciation class was deleted successfully.'
+    )
 
 );
