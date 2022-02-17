813810b3a92d4184f0f1f233147f8f90d8efc67a
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 18:45:44 -0700

    New translations table.php (Afrikaans) Fixed: New translations table.php from Crowdin

--- resources/lang/af-ZA/table.php
+++ resources/lang/af-ZA/table.php
@@ -1,10 +1,11 @@
 <?php
 
-return array(
+return [
 
-    'actions'	 	=> 'Actions',
-    'action' 		=> 'Action',
-    'by'      		=> 'By',
-    'item' 			=> 'Item',
+    'id'      => 'ID',
+    'months'   => 'Months',
+    'term'   => 'Term',
+    'title'      => 'Name ',
+    'depreciation_min' => 'Floor Value',
 
-);
+];
