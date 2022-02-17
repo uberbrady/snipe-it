54f4f7bee0bcc49d0205df4d169672d40f862f3c
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 20:13:07 -0700

    New translations table.php (Swedish) Fixed: New translations table.php from Crowdin

--- resources/lang/sv-SE/table.php
+++ resources/lang/sv-SE/table.php
@@ -2,16 +2,9 @@
 
 return array(
 
-    'create'				=> 'Create Asset Model',
-    'created_at' 			=> 'Created at',
-    'eol'	 				=> 'EOL',
-    'modelnumber'   		=> 'Model No.',
-    'name'      			=> 'Asset Model Name',
-    'numassets' 			=> 'Assets',
-    'title'					=> 'Asset Models',
-    'update'				=> 'Update Asset Model',
-    'view'					=> 'View Asset Model',
-    'update'				=> 'Update Asset Model',
-    'clone'				=> 'Clone Model',
-    'edit'				=> 'Edit Model',
+    'actions'	 	=> 'Åtgärder',
+    'action' 		=> 'Åtgärd',
+    'by'      		=> 'Av',
+    'item' 			=> 'Artikel',
+
 );
