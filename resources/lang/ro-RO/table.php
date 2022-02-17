60a5cfc12dab29fad63500274e485e00f7a5eb1e
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 19:13:27 -0700

    New translations table.php (Romanian) Fixed: New translations table.php from Crowdin

--- resources/lang/ro-RO/table.php
+++ resources/lang/ro-RO/table.php
@@ -1,11 +1,10 @@
 <?php
 
 return array(
-	'dl_csv'      				=> 'Download CSV',
 	'eula_text'      			=> 'EULA',
     'id'      					=> 'ID',
+    'parent'   					=> 'Parent',
     'require_acceptance'      	=> 'Acceptance',
-    'title'      				=> 'Accessory Name',
-
+    'title'      				=> 'Asset Category Name',
 
 );
