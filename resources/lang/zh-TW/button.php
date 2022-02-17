23ed251175013cc8d9233505bd173f9902c4c8c1
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 20:17:53 -0700

    New translations button.php (Chinese Traditional) Fixed: New translations button.php from Crowdin

--- resources/lang/zh-TW/button.php
+++ resources/lang/zh-TW/button.php
@@ -1,6 +1,6 @@
 <?php
 
-return array(
+return [
     'actions' 	                => '操作',
     'add'    	                => '新增',
     'cancel'                    => '取消',
@@ -8,11 +8,17 @@ return array(
     'delete'  	                => '刪除',
     'edit'    	                => '編輯',
     'restore' 	                => '還原',
+    'remove'                    => 'Remove',
     'request'                   => '申請',
     'submit'  	                => '送出',
     'upload'                    => '上傳',
-	'select_file'				=> '選擇檔案…',
+    'select_file'				=> '選擇檔案…',
     'select_files'				=> '選擇檔案…',
     'generate_labels'           => '{1} Generate Label|[2,*] Generate Labels',
-    'send_password_link'       => 'Send Password Reset Link',
-);
+    'send_password_link'        => 'Send Password Reset Link',
+    'go'                        => 'Go',
+    'bulk_actions'              => 'Bulk Actions',
+    'add_maintenance'           => 'Add Maintenance',
+    'append'                    => 'Append',
+    'new'                       => 'New',
+];
