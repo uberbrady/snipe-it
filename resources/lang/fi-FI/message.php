da18abf194a6fda874841b5140cbb409b6521db9
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 18:52:46 -0700

    New translations message.php (Finnish) Fixed: New translations message.php from Crowdin

--- resources/lang/fi-FI/message.php
+++ resources/lang/fi-FI/message.php
@@ -1,36 +1,18 @@
 <?php
-
 return array(
-
-    'does_not_exist' => 'Consumable does not exist.',
-
+    'does_not_exist' => 'Company does not exist.',
+    'assoc_users'    => 'This company is currently associated with at least one model and cannot be deleted. Please update your models to no longer reference this company and try again. ',
     'create' => array(
-        'error'   => 'Consumable was not created, please try again.',
-        'success' => 'Consumable created successfully.'
+        'error'   => 'Company was not created, please try again.',
+        'success' => 'Company created successfully.'
     ),
-
     'update' => array(
-        'error'   => 'Consumable was not updated, please try again',
-        'success' => 'Consumable updated successfully.'
+        'error'   => 'Company was not updated, please try again',
+        'success' => 'Company updated successfully.'
     ),
-
     'delete' => array(
-        'confirm'   => 'Are you sure you wish to delete this consumable?',
-        'error'   => 'There was an issue deleting the consumable. Please try again.',
-        'success' => 'The consumable was deleted successfully.'
-    ),
-
-     'checkout' => array(
-        'error'   		=> 'Consumable was not checked out, please try again',
-        'success' 		=> 'Consumable checked out successfully.',
-        'user_does_not_exist' => 'That user is invalid. Please try again.'
-    ),
-
-    'checkin' => array(
-        'error'   		=> 'Consumable was not checked in, please try again',
-        'success' 		=> 'Consumable checked in successfully.',
-        'user_does_not_exist' => 'That user is invalid. Please try again.'
+        'confirm' => 'Are you sure you wish to delete this company?',
+        'error'   => 'There was an issue deleting the company. Please try again.',
+        'success' => 'The Company was deleted successfully.'
     )
-
-
 );
