6b75e4709bccfd87caed34b4d3a465cf02d96b8c
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 19:00:56 -0700

    New translations general.php (Polish) Fixed: New translations general.php from Crowdin

--- resources/lang/pl-PL/general.php
+++ resources/lang/pl-PL/general.php
@@ -1,7 +1,11 @@
 <?php
 
-return [
-    'select_company' => 'Select Company',
-    'about_companies' => 'About Companies',
-    'about_companies_description' => '        You can use companies as a simple informative field, or you can use them to restrict asset visibility and availability to users with a specific company by enabling Full Company Support in your Admin Settings.',
-];
+return array(
+    'checkout'                          => 'Checkout Consumable to User',
+    'consumable_name'                   => 'Consumable Name',
+    'create'                            => 'Create Consumable',
+    'item_no'                           => 'Item No.',
+    'remaining' 			            => 'Remaining',
+    'total' 			                => 'Total',
+    'update'                            => 'Update Consumable',
+);
