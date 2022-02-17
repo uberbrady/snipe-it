6a67ef9481526f9b0fa9815798ef53784f1117b8
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 20:08:15 -0700

    New translations table.php (Polish) Fixed: New translations table.php from Crowdin

--- resources/lang/pl-PL/table.php
+++ resources/lang/pl-PL/table.php
@@ -1,16 +1,10 @@
 <?php
 
 return array(
-    'about_manufacturers_title'    => 'About manufacturers',
-    'about_manufacturers_text'  => 'Manufacturers are the companies that create your assets. You can store important support contact information about them here, which will be displayed on your asset detail pages.',
-    'asset_manufacturers'	=> 'Asset Manufacturers',
-    'create'				=> 'Create Manufacturer',
-    'id'   					=> 'ID',
-    'name'      			=> 'Name',
-    'support_email'   		=> 'Support Email',
-    'support_phone'   		=> 'Support Phone',
-    'support_url'   		=> 'Support URL',
-    'update'				=> 'Update Manufacturer',
-    'url'   				=> 'URL',
+	'eula_text'      			=> 'EULA',
+    'id'      					=> 'ID',
+    'parent'   					=> 'Parent',
+    'require_acceptance'      	=> 'Acceptance',
+    'title'      				=> 'Asset Category Name',
 
 );
