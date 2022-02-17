c490f5ae7803cb04ba6614dc900b3503f1144087
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 18:48:02 -0700

    New translations message.php (Catalan) Fixed: New translations message.php from Crowdin

--- resources/lang/ca-ES/message.php
+++ resources/lang/ca-ES/message.php
@@ -2,24 +2,26 @@
 
 return array(
 
-    'does_not_exist' => 'Category does not exist.',
-    'assoc_models'	 => 'This category is currently associated with at least one model and cannot be deleted. Please update your models to no longer reference this category and try again. ',
-    'assoc_items'	 => 'This category is currently associated with at least one :asset_type and cannot be deleted. Please update your :asset_type  to no longer reference this category and try again. ',
+    'does_not_exist' => 'Location does not exist.',
+    'assoc_users'	 => 'This location is currently associated with at least one user and cannot be deleted. Please update your users to no longer reference this location and try again. ',
+    'assoc_assets'	 => 'This location is currently associated with at least one asset and cannot be deleted. Please update your assets to no longer reference this location and try again. ',
+    'assoc_child_loc'	 => 'This location is currently the parent of at least one child location and cannot be deleted. Please update your locations to no longer reference this location and try again. ',
+
 
     'create' => array(
-        'error'   => 'Category was not created, please try again.',
-        'success' => 'Category created successfully.'
+        'error'   => 'Location was not created, please try again.',
+        'success' => 'Location created successfully.'
     ),
 
     'update' => array(
-        'error'   => 'Category was not updated, please try again',
-        'success' => 'Category updated successfully.'
+        'error'   => 'Location was not updated, please try again',
+        'success' => 'Location updated successfully.'
     ),
 
     'delete' => array(
-        'confirm'   => 'Are you sure you wish to delete this category?',
-        'error'   => 'There was an issue deleting the category. Please try again.',
-        'success' => 'The category was deleted successfully.'
+        'confirm'   	=> 'Are you sure you wish to delete this location?',
+        'error'   => 'There was an issue deleting the location. Please try again.',
+        'success' => 'The location was deleted successfully.'
     )
 
 );
