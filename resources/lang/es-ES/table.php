a1555a870ebfe527fa5e33a8b2e7b9400b33afda
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 19:15:09 -0700

    New translations table.php (Spanish) Fixed: New translations table.php from Crowdin

--- resources/lang/es-ES/table.php
+++ resources/lang/es-ES/table.php
@@ -1,16 +1,8 @@
 <?php
 
-return array(
-    'about_manufacturers_title'    => 'About manufacturers',
-    'about_manufacturers_text'  => 'Manufacturers are the companies that create your assets. You can store important support contact information about them here, which will be displayed on your asset detail pages.',
-    'asset_manufacturers'	=> 'Asset Manufacturers',
-    'create'				=> 'Create Manufacturer',
-    'id'   					=> 'ID',
-    'name'      			=> 'Name',
-    'support_email'   		=> 'Support Email',
-    'support_phone'   		=> 'Support Phone',
-    'support_url'   		=> 'Support URL',
-    'update'				=> 'Update Manufacturer',
-    'url'   				=> 'URL',
-
-);
+    return [
+        'title'         => 'Asset Maintenance',
+        'asset_name'    => 'Asset Name',
+        'is_warranty'   => 'Warranty',
+        'dl_csv'        => 'Download CSV'
+    ];
