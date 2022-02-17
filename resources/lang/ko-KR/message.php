4e5330e9601c655c7bae9f494dc88d54bdd8ed81
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 20:05:10 -0700

    New translations message.php (Korean) Fixed: New translations message.php from Crowdin

--- resources/lang/ko-KR/message.php
+++ resources/lang/ko-KR/message.php
@@ -2,35 +2,24 @@
 
 return array(
 
-    'does_not_exist' => 'Component does not exist.',
+    'does_not_exist' => 'Category does not exist.',
+    'assoc_models'	 => 'This category is currently associated with at least one model and cannot be deleted. Please update your models to no longer reference this category and try again. ',
+    'assoc_items'	 => 'This category is currently associated with at least one :asset_type and cannot be deleted. Please update your :asset_type  to no longer reference this category and try again. ',
 
     'create' => array(
-        'error'   => 'Component was not created, please try again.',
-        'success' => 'Component created successfully.'
+        'error'   => 'Category was not created, please try again.',
+        'success' => 'Category created successfully.'
     ),
 
     'update' => array(
-        'error'   => 'Component was not updated, please try again',
-        'success' => 'Component updated successfully.'
+        'error'   => 'Category was not updated, please try again',
+        'success' => 'Category updated successfully.'
     ),
 
     'delete' => array(
-        'confirm'   => 'Are you sure you wish to delete this component?',
-        'error'   => 'There was an issue deleting the component. Please try again.',
-        'success' => 'The component was deleted successfully.'
-    ),
-
-     'checkout' => array(
-        'error'   		=> 'Component was not checked out, please try again',
-        'success' 		=> 'Component checked out successfully.',
-        'user_does_not_exist' => 'That user is invalid. Please try again.'
-    ),
-
-    'checkin' => array(
-        'error'   		=> 'Component was not checked in, please try again',
-        'success' 		=> 'Component checked in successfully.',
-        'user_does_not_exist' => 'That user is invalid. Please try again.'
+        'confirm'   => 'Are you sure you wish to delete this category?',
+        'error'   => 'There was an issue deleting the category. Please try again.',
+        'success' => 'The category was deleted successfully.'
     )
-
 
 );
