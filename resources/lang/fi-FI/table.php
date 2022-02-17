102de2da5b27ff47e4dc34eda063e0c58fa6be13
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 18:53:43 -0700

    New translations table.php (Finnish) Fixed: New translations table.php from Crowdin

--- resources/lang/fi-FI/table.php
+++ resources/lang/fi-FI/table.php
@@ -2,8 +2,16 @@
 
 return array(
 
-    'id'         => 'Id',
-    'name'       => 'Name',
-    'users'      => '# of Users',
-
+    'create'				=> 'Create Asset Model',
+    'created_at' 			=> 'Created at',
+    'eol'	 				=> 'EOL',
+    'modelnumber'   		=> 'Model No.',
+    'name'      			=> 'Asset Model Name',
+    'numassets' 			=> 'Assets',
+    'title'					=> 'Asset Models',
+    'update'				=> 'Update Asset Model',
+    'view'					=> 'View Asset Model',
+    'update'				=> 'Update Asset Model',
+    'clone'				=> 'Clone Model',
+    'edit'				=> 'Edit Model',
 );
