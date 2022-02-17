a63c31e6bfb70ab7e6b45fce3b9041b088bc11e2
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 19:03:45 -0700

    New translations message.php (Russian) Fixed: New translations message.php from Crowdin

--- resources/lang/ru-RU/message.php
+++ resources/lang/ru-RU/message.php
@@ -1,18 +1,29 @@
 <?php
+
 return array(
-    'does_not_exist' => 'Company does not exist.',
-    'assoc_users'    => 'This company is currently associated with at least one model and cannot be deleted. Please update your models to no longer reference this company and try again. ',
+
+    'does_not_exist' => 'Manufacturer does not exist.',
+    'assoc_users'	 => 'This manufacturer is currently associated with at least one model and cannot be deleted. Please update your models to no longer reference this manufacturer and try again. ',
+
     'create' => array(
-        'error'   => 'Company was not created, please try again.',
-        'success' => 'Company created successfully.'
+        'error'   => 'Manufacturer was not created, please try again.',
+        'success' => 'Manufacturer created successfully.'
     ),
+
     'update' => array(
-        'error'   => 'Company was not updated, please try again',
-        'success' => 'Company updated successfully.'
+        'error'   => 'Manufacturer was not updated, please try again',
+        'success' => 'Manufacturer updated successfully.'
     ),
+
+    'restore' => array(
+        'error'   => 'Manufacturer was not restored, please try again',
+        'success' => 'Manufacturer restored successfully.'
+    ),
+
     'delete' => array(
-        'confirm' => 'Are you sure you wish to delete this company?',
-        'error'   => 'There was an issue deleting the company. Please try again.',
-        'success' => 'The Company was deleted successfully.'
+        'confirm'   => 'Are you sure you wish to delete this manufacturer?',
+        'error'   => 'There was an issue deleting the manufacturer. Please try again.',
+        'success' => 'The Manufacturer was deleted successfully.'
     )
+
 );
