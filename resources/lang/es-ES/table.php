7d6740988d5694cc81a259d0e7816d8d586a00de
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 18:45:40 -0700

    New translations table.php (Spanish) Fixed: New translations table.php from Crowdin

--- resources/lang/es-ES/table.php
+++ resources/lang/es-ES/table.php
@@ -2,16 +2,9 @@
 
 return array(
 
-    'assigned_to'   	=> 'Assigned To',
-    'checkout'   		=> 'In/Out',
-    'id'      			=> 'ID',
-    'license_email'   	=> 'License Email',
-    'license_name'   	=> 'Licensed To',
-    'purchase_date'   	=> 'Purchase Date',
-    'purchased'   		=> 'Purchased',
-    'seats'   			=> 'Seats',
-    'hardware'   		=> 'Hardware',
-    'serial'   			=> 'Serial',
-    'title'      		=> 'License',
+    'actions'	 	=> 'Acciones',
+    'action' 		=> 'Acción',
+    'by'      		=> 'Por',
+    'item' 			=> 'Item',
 
 );
