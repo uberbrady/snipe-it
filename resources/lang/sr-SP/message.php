2cf891c916c2a29d4b0482729016a7387bbd04df
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 19:04:40 -0700

    New translations message.php (Serbian (Cyrillic)) Fixed: New translations message.php from Crowdin

--- resources/lang/sr-SP/message.php
+++ resources/lang/sr-SP/message.php
@@ -2,44 +2,21 @@
 
 return array(
 
-    'account_already_exists' => 'An account with the this email already exists.',
-    'account_not_found'      => 'The username or password is incorrect.',
-    'account_not_activated'  => 'This user account is not activated.',
-    'account_suspended'      => 'This user account is suspended.',
-    'account_banned'         => 'This user account is banned.',
-    'throttle'               => 'Too many failed login attempts. Please try again in :minutes minutes.',
 
-    'two_factor' => array(
-        'already_enrolled'      => 'Your device is already enrolled.',
-        'success'               => 'You have successfully logged in.',
-        'code_required'         => 'Two-factor code is required.',
-        'invalid_code'          => 'Two-factor code is invalid.',
+    'update' => array(
+        'error'                 => 'An error has occurred while updating. ',
+        'success'               => 'Settings updated successfully.'
     ),
-
-    'signin' => array(
-        'error'   => 'There was a problem while trying to log you in, please try again.',
-        'success' => 'You have successfully logged in.',
+    'backup' => array(
+        'delete_confirm'        => 'Are you sure you would like to delete this backup file? This action cannot be undone. ',
+        'file_deleted'          => 'The backup file was successfully deleted. ',
+        'generated'             => 'A new backup file was successfully created.',
+        'file_not_found'        => 'That backup file could not be found on the server.',
     ),
-
-    'logout' => array(
-        'error'   => 'There was a problem while trying to log you out, please try again.',
-        'success' => 'You have successfully logged out.',
+    'purge' => array(
+        'error'     => 'An error has occurred while purging. ',
+        'validation_failed'     => 'Your purge confirmation is incorrect. Please type the word "DELETE" in the confirmation box.',
+        'success'               => 'Deleted records successfully purged.'
     ),
-
-    'signup' => array(
-        'error'   => 'There was a problem while trying to create your account, please try again.',
-        'success' => 'Account sucessfully created.',
-    ),
-
-    'forgot-password' => array(
-        'error'   => 'There was a problem while trying to get a reset password code, please try again.',
-        'success' => 'If that email address exists in our system, a password recovery email has been sent.',
-    ),
-
-    'forgot-password-confirm' => array(
-        'error'   => 'There was a problem while trying to reset your password, please try again.',
-        'success' => 'Your password has been successfully reset.',
-    ),
-
 
 );
