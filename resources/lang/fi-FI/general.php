bf1201e10cef3943436be226e55fdae6f50197ce
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 20:00:20 -0700

    New translations general.php (Finnish) Fixed: New translations general.php from Crowdin

--- resources/lang/fi-FI/general.php
+++ resources/lang/fi-FI/general.php
@@ -1,14 +1,17 @@
 <?php
 
-    return [
-        'asset_maintenances' => 'Asset Maintenances',
-        'edit'               => 'Edit Asset Maintenance',
-        'delete'             => 'Delete Asset Maintenance',
-        'view'               => 'View Asset Maintenance Details',
-        'repair'             => 'Repair',
-        'maintenance'        => 'Maintenance',
-        'upgrade'            => 'Upgrade',
-        'calibration'        => 'Calibration',
-        'software_support'   => 'Software Support',
-        'hardware_support'   => 'Hardware Support',
-    ];
+return array(
+    'about_kits_title'                  => 'About Predefined Kits',
+    'about_kits_text'                   => 'Predefined Kits let you quickly check out a collection of items (assets, licenses, etc) to a user. This can be helpful when your onboarding process is consistent across many users and all users receive the same items.',
+    'checkout'                          => 'Checkout Kit ',
+    'create_success'                    => 'Kit was successfully created.',
+    'create'                            => 'Create Predefined Kit',
+    'update'                            => 'Update Predefined Kit',
+    'delete_success'                    => 'Kit was successfully deleted.',
+    'update_success'                    => 'Kit was successfully updated.',
+    'none_models'                       => 'There are not enough available assets for :model to checkout. :qty are required. ',
+    'none_licenses'                     => 'There are not enough available seats for :license to checkout. :qty are required. ',
+    'none_consumables'                  => 'There are not enough available units of :consumable to checkout. :qty are required. ',
+    'none_accessory'                    => 'There are not enough available units of :accessory to checkout. :qty are required. ',
+
+);
