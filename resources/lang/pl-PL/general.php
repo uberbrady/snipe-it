d42491d57f42c8d0fb7281dd3672f1a61a026d67
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 20:08:53 -0700

    New translations general.php (Polish) Fixed: New translations general.php from Crowdin

--- resources/lang/pl-PL/general.php
+++ resources/lang/pl-PL/general.php
@@ -1,23 +1,20 @@
 <?php
 
 return array(
-    'asset_categories' 					=> 'Asset Categories',
-    'category_name'  					=> 'Category Name',
-    'checkin_email'                     => 'Send email to user on checkin/checkout.',
-    'checkin_email_notification'        => 'This user will be sent an email on checkin/checkout.',
-    'clone'                             => 'Clone Category',
-    'create'  							=> 'Create Category',
-    'edit'                              => 'Edit Category',
+    'accessory_category' 				=> 'Accessory Category',
+    'accessory_name'  					=> 'Accessory Name',
+    'checkout'  							=> 'Checkout Accessory',
+    'checkin'  							=> 'Checkin Accessory',
+    'create'  							=> 'Create Accessory',
+    'edit'  							=> 'Edit Accessory',
     'eula_text'							=> 'Category EULA',
     'eula_text_help'					=> 'This field allows you to customize your EULAs for specific types of assets. If you only have one EULA for all of your assets, you can check the box below to use the primary default.',
-    'name'                              => 'Category Name',
     'require_acceptance'				=> 'Require users to confirm acceptance of assets in this category.',
-    'required_acceptance'				=> 'This user will be emailed with a link to confirm acceptance of this item.',
-    'required_eula'						=> 'This user will be emailed a copy of the EULA',
     'no_default_eula'					=> 'No primary default EULA found. Add one in Settings.',
-    'update'  							=> 'Update Category',
+    'total'  							=> 'Total',
+    'remaining'  						=> 'Avail',
+    'update'  							=> 'Update Accessory',
     'use_default_eula'					=> 'Use the <a href="#" data-toggle="modal" data-target="#eulaModal">primary default EULA</a> instead.',
     'use_default_eula_disabled'			=> '<del>Use the primary default EULA instead.</del> No primary default EULA is set. Please add one in Settings.',
-    'use_default_eula_column'			=> 'Use default EULA',
 
 );
