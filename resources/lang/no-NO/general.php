7f4e81cee8d9cc7b0a2b3e774b309760200d70ef
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 20:08:11 -0700

    New translations general.php (Norwegian) Fixed: New translations general.php from Crowdin

--- resources/lang/no-NO/general.php
+++ resources/lang/no-NO/general.php
@@ -1,11 +1,5 @@
 <?php
 
 return array(
-    'checkout'                          => 'Checkout Consumable to User',
-    'consumable_name'                   => 'Consumable Name',
-    'create'                            => 'Create Consumable',
-    'item_no'                           => 'Item No.',
-    'remaining' 			            => 'Remaining',
-    'total' 			                => 'Total',
-    'update'                            => 'Update Consumable',
+    'info'   => 'Select the options you want for your asset report.'
 );
