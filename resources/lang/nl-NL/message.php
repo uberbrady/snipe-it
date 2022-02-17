75b1ff169f1a6ac872bdb7460ea090dea325828b
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 18:59:13 -0700

    New translations message.php (Dutch) Fixed: New translations message.php from Crowdin

--- resources/lang/nl-NL/message.php
+++ resources/lang/nl-NL/message.php
@@ -1,31 +1,29 @@
 <?php
 
-return [
+return array(
 
-    'does_not_exist' => 'Status Label does not exist.',
-    'assoc_assets'	 => 'This Status Label is currently associated with at least one Asset and cannot be deleted. Please update your assets to no longer reference this status and try again. ',
+    'does_not_exist' => 'Manufacturer does not exist.',
+    'assoc_users'	 => 'This manufacturer is currently associated with at least one model and cannot be deleted. Please update your models to no longer reference this manufacturer and try again. ',
 
-    'create' => [
-        'error'   => 'Status Label was not created, please try again.',
-        'success' => 'Status Label created successfully.',
-    ],
+    'create' => array(
+        'error'   => 'Manufacturer was not created, please try again.',
+        'success' => 'Manufacturer created successfully.'
+    ),
 
-    'update' => [
-        'error'   => 'Status Label was not updated, please try again',
-        'success' => 'Status Label updated successfully.',
-    ],
+    'update' => array(
+        'error'   => 'Manufacturer was not updated, please try again',
+        'success' => 'Manufacturer updated successfully.'
+    ),
 
-    'delete' => [
-        'confirm'   => 'Are you sure you wish to delete this Status Label?',
-        'error'   => 'There was an issue deleting the Status Label. Please try again.',
-        'success' => 'The Status Label was deleted successfully.',
-    ],
+    'restore' => array(
+        'error'   => 'Manufacturer was not restored, please try again',
+        'success' => 'Manufacturer restored successfully.'
+    ),
 
-    'help' => [
-        'undeployable'   => 'These assets cannot be assigned to anyone.',
-        'deployable'   => 'These assets can be checked out. Once they are assigned, they will assume a meta status of <i class="fas fa-circle text-blue"></i> <strong>Deployed</strong>.',
-        'archived'   => 'These assets cannot be checked out, and will only show up in the Archived view. This is useful for retaining information about assets for budgeting/historic purposes but keeping them out of the day-to-day asset list.',
-        'pending'   => 'These assets can not yet be assigned to anyone, often used for items that are out for repair, but are expected to return to circulation.',
-    ],
+    'delete' => array(
+        'confirm'   => 'Are you sure you wish to delete this manufacturer?',
+        'error'   => 'There was an issue deleting the manufacturer. Please try again.',
+        'success' => 'The Manufacturer was deleted successfully.'
+    )
 
-];
+);
