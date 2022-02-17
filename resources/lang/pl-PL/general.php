6cc1d114782339516f0b0ac41d804928bfb88df7
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 20:09:21 -0700

    New translations general.php (Polish) Fixed: New translations general.php from Crowdin

--- resources/lang/pl-PL/general.php
+++ resources/lang/pl-PL/general.php
@@ -1,18 +1,17 @@
 <?php
 
 return array(
-    'about_models_title'     => 'About Asset Models',
-    'about_models_text'           => 'Asset Models are a way to group identical assets. "MBP 2013", "IPhone 6s", etc.',
-    'deleted'  					        => 'This model has been deleted.',
-    'bulk_delete'               => 'Bulk Delete Asset Models',
-    'bulk_delete_help'               => 'Use the checkboxes below to confirm the deletion of the selected asset models. Asset models that have assets associated with them cannot be deleted until the assets are associated with a different model.',
-    'bulk_delete_warn'          => 'You are about to delete :model_count asset models.',
-    'restore'                   => 'Restore Model',
-    'requestable'               => 'Users may request this model',
-	'show_mac_address'			      => 'Show MAC address field in assets in this model',
-    'view_deleted'              => 'View Deleted',
-    'view_models'               => 'View Models',
-    'fieldset'                  => 'Fieldset',
-    'no_custom_field'           => 'No custom fields',
-    'add_default_values'        => 'Add default values',
+    'about_kits_title'                  => 'About Predefined Kits',
+    'about_kits_text'                   => 'Predefined Kits let you quickly check out a collection of items (assets, licenses, etc) to a user. This can be helpful when your onboarding process is consistent across many users and all users receive the same items.',
+    'checkout'                          => 'Checkout Kit ',
+    'create_success'                    => 'Kit was successfully created.',
+    'create'                            => 'Create Predefined Kit',
+    'update'                            => 'Update Predefined Kit',
+    'delete_success'                    => 'Kit was successfully deleted.',
+    'update_success'                    => 'Kit was successfully updated.',
+    'none_models'                       => 'There are not enough available assets for :model to checkout. :qty are required. ',
+    'none_licenses'                     => 'There are not enough available seats for :license to checkout. :qty are required. ',
+    'none_consumables'                  => 'There are not enough available units of :consumable to checkout. :qty are required. ',
+    'none_accessory'                    => 'There are not enough available units of :accessory to checkout. :qty are required. ',
+
 );
