3de80dace749b381b117528e26b3b113b76fff7f
Brady Wetherington <uberbrady@gmail.com>
2022-02-16 20:04:49 -0700

    New translations general.php (Japanese) Fixed: New translations general.php from Crowdin

--- resources/lang/ja-JP/general.php
+++ resources/lang/ja-JP/general.php
@@ -1,14 +1,4 @@
 <?php
-
-    return [
-        'asset_maintenances' => 'Asset Maintenances',
-        'edit'               => 'Edit Asset Maintenance',
-        'delete'             => 'Delete Asset Maintenance',
-        'view'               => 'View Asset Maintenance Details',
-        'repair'             => 'Repair',
-        'maintenance'        => 'Maintenance',
-        'upgrade'            => 'Upgrade',
-        'calibration'        => 'Calibration',
-        'software_support'   => 'Software Support',
-        'hardware_support'   => 'Hardware Support',
-    ];
+return [
+    'select_company' => 'Select Company',
+];
