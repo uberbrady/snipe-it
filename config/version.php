2ad3d1d832144f670dc7a547f010f04cad880dd0
Brady Wetherington <bwetherington@grokability.com>
2025-02-24 18:01:43 +0000

    Tweaking version numbers.

--- config/version.php
+++ config/version.php
@@ -1,10 +1,10 @@
 <?php
 return array (
-    'app_version'      => 'v8.0.0-pre',
-    'full_app_version' => 'v8.0.0-pre - build 16501-gca11efd3ee',
-    'build_version'    => '16501',
-  'prerelease_version' => '',
-    'hash_version'     => 'gca11efd3ee',
-    'full_hash'        => 'v8.0.0-pre-447-gca11efd3ee',
-  'branch' => 'develop',
+    'app_version'        => 'v8.0.0',
+    'full_app_version'   => 'v8.0.0 - build 16958-g5fa4f85c20',
+    'build_version'      => '16958',
+    'prerelease_version' => '',
+    'hash_version'       => 'g5fa4f85c20',
+    'full_hash'          => 'v8.0.0-383-g5fa4f85c20',
+    'branch'             => 'master',
 );
