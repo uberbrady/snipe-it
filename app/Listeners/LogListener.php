819031efb4e6d6219c7454fff004bc570088e8cb
Brady Wetherington <bwetherington@grokability.com>
2025-08-28 18:07:25 +0100

    Change merge action to live as a user method and invoke it directly

--- app/Listeners/LogListener.php
+++ app/Listeners/LogListener.php
@@ -7,8 +7,6 @@ use App\Events\AccessoryCheckedIn;
 use App\Events\AccessoryCheckedOut;
 use App\Events\AssetCheckedIn;
 use App\Events\AssetCheckedOut;
-use App\Events\CheckoutableCheckedIn;
-use App\Events\CheckoutableCheckedOut;
 use App\Events\CheckoutAccepted;
 use App\Events\CheckoutDeclined;
 use App\Events\ComponentCheckedIn;
@@ -19,9 +17,7 @@ use App\Events\ItemDeclined;
 use App\Events\LicenseCheckedIn;
 use App\Events\LicenseCheckedOut;
 use App\Models\Actionlog;
-use App\Models\User;
 use App\Models\LicenseSeat;
-use App\Events\UserMerged;
 use Illuminate\Support\Facades\Log;
 
 class LogListener
@@ -70,41 +66,6 @@ class LogListener
     }
 
 
-    public function onUserMerged(UserMerged $event)
-    {
-
-        $to_from_array = [
-            'to_id' => $event->merged_to->id,
-            'to_username' => $event->merged_to->username,
-            'from_id' => $event->merged_from->id,
-            'from_username' => $event->merged_from->username,
-        ];
-
-        // Add a record to the users being merged FROM
-        Log::debug('Users merged: '.$event->merged_from->id .' ('.$event->merged_from->username.') merged into '. $event->merged_to->id. ' ('.$event->merged_to->username.')');
-        $logaction = new Actionlog();
-        $logaction->item_id = $event->merged_from->id;
-        $logaction->item_type = User::class;
-        $logaction->target_id = $event->merged_to->id;
-        $logaction->target_type = User::class;
-        $logaction->action_type = 'merged';
-        $logaction->note = trans('general.merged_log_this_user_from', $to_from_array);
-        $logaction->created_by = $event->admin->id ?? null;
-        $logaction->save();
-
-        // Add a record to the users being merged TO
-        $logaction = new Actionlog();
-        $logaction->target_id = $event->merged_from->id;
-        $logaction->target_type = User::class;
-        $logaction->item_id = $event->merged_to->id;
-        $logaction->item_type = User::class;
-        $logaction->action_type = 'merged';
-        $logaction->note = trans('general.merged_log_this_user_into', $to_from_array);
-        $logaction->created_by = $event->admin->id ?? null;
-        $logaction->save();
-
-
-    }
 
     /**
      * Register the listeners for the subscriber.
@@ -116,8 +77,6 @@ class LogListener
         $list = [
             'CheckoutAccepted',
             'CheckoutDeclined',
-            'UserMerged',
-            'NoteAdded',
         ];
 
         foreach ($list as $event) {
