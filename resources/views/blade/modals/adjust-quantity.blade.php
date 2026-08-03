91318416a4ed262f3c2d80d910c7339c4f01206f
snipe <snipe@snipe.net>
2026-08-03 13:15:06 +0100

    Switch to horizonal form for qty modal

--- resources/views/blade/modals/adjust-quantity.blade.php
+++ resources/views/blade/modals/adjust-quantity.blade.php
@@ -36,7 +36,35 @@
                         <textarea class="form-control" id="adjustQuantityNote" name="note" rows="3" maxlength="65535" required></textarea>
                     </div>
 
-                    <x-input.file-upload inputId="adjustQuantityFile" name="file" :multiple="false"/>
+                    {{-- Inlined instead of <x-input.file-upload> because that
+                         component wraps its markup in <x-form.row>, a Bootstrap 3
+                         horizontal-form scaffold (col-md-3 label + col-md-9
+                         input). The three fields above use vertical form-group
+                         styling, so the horizontal scaffold collided with the
+                         modal-footer top border. --}}
+                    <div class="form-group">
+                        <label for="adjustQuantityFile">{{ trans('general.file_upload') }}</label>
+                        <div>
+                            <label class="btn btn-sm btn-theme" for="adjustQuantityFile">
+                                {{ trans('button.select_file') }}
+                                <input
+                                    type="file"
+                                    name="file"
+                                    class="js-uploadFile"
+                                    id="adjustQuantityFile"
+                                    data-maxsize="{{ \App\Helpers\Helper::file_upload_max_size() }}"
+                                    accept="{{ config('filesystems.allowed_upload_mimetypes') }}"
+                                    style="display:none"
+                                    aria-label="file"
+                                    aria-hidden="true"
+                                >
+                            </label>
+                            <span id="adjustQuantityFile-info"></span>
+                            <p class="help-block" id="adjustQuantityFile-status">
+                                {{ trans('general.upload_filetypes_help', ['allowed_filetypes' => config('filesystems.allowed_upload_extensions'), 'size' => \App\Helpers\Helper::file_upload_max_size_readable()]) }}
+                            </p>
+                        </div>
+                    </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-default pull-left" data-dismiss="modal">{{ trans('button.cancel') }}</button>
