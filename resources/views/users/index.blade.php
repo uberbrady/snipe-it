6ff95ba5cf5ac7c8dd09664aa7cdbac8ed384900
snipe <snipe@snipe.net>
2026-08-07 14:01:31 +0100

    Hide button in blade for non-superusers

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -18,11 +18,12 @@
 
 @section('header_right')
 
-    @can('create', \App\Models\User::class)
-        @if ($snipeSettings->ldap_enabled == 1)
-            <a href="{{ route('ldap/user') }}" class="btn btn-theme pull-right"><i class="fas fa-sitemap"></i> {{trans('general.ldap_sync')}}</a>
-        @endif
-    @endcan
+    {{-- LDAP sync surfaces users from across the entire directory
+         regardless of company scoping, so the button is superuser-only
+         to match LDAPImportController's authorization. --}}
+    @if (auth()->user()?->isSuperUser() && $snipeSettings->ldap_enabled == 1)
+        <a href="{{ route('ldap/user') }}" class="btn btn-theme pull-right"><i class="fas fa-sitemap"></i> {{trans('general.ldap_sync')}}</a>
+    @endif
 @stop
 
 {{-- Page content --}}
