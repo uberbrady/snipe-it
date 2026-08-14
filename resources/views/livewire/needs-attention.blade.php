31a18856f4c21214eb4b38c022d333acef30eb0c
snipe <snipe@snipe.net>
2026-08-14 17:12:20 +0100

    Added type icons

--- resources/views/livewire/needs-attention.blade.php
+++ resources/views/livewire/needs-attention.blade.php
@@ -18,6 +18,7 @@
             @can('audit', \App\Models\Asset::class)
                 <li class="list-group-item">
                     <a href="{{ route('assets.audit.due') }}">
+                        <x-icon type="asset" class="fa-fw"/>
                         <span class="dashboard-attention-label">{{ trans('general.dashboard_overdue_audits') }}</span>
                         <span class="badge dashboard-attention-count">{{ number_format($overdueAudits) }}</span>
                     </a>
@@ -26,21 +27,15 @@
             @can('checkin', \App\Models\Asset::class)
                 <li class="list-group-item">
                     <a href="{{ route('assets.checkins.due') }}">
+                        <x-icon type="asset" class="fa-fw"/>
                         <span class="dashboard-attention-label">{{ trans('general.dashboard_overdue_checkins') }}</span>
                         <span class="badge dashboard-attention-count">{{ number_format($overdueCheckins) }}</span>
                     </a>
                 </li>
             @endcan
-            @can('view', \App\Models\Asset::class)
-                <li class="list-group-item">
-                    <a href="{{ route('maintenances.index', ['upcoming_status' => 'overdue']) }}">
-                        <span class="dashboard-attention-label">{{ trans('general.dashboard_overdue_maintenances') }}</span>
-                        <span class="badge dashboard-attention-count">{{ number_format($overdueMaintenances) }}</span>
-                    </a>
-                </li>
-            @endcan
             <li class="list-group-item">
                 <a href="{{ route('reports/unaccepted_assets') }}">
+                    <x-icon type="asset" class="fa-fw"/>
                     <span class="dashboard-attention-label">{{ trans('general.dashboard_unaccepted_assets') }}</span>
                     <span class="badge dashboard-attention-count">{{ number_format($pendingAcceptancesCount) }}</span>
                 </a>
@@ -48,30 +43,42 @@
             @canany(['checkout', 'checkin'], \App\Models\Asset::class)
                 <li class="list-group-item">
                     <a href="{{ route('assets.requested') }}">
+                        <x-icon type="asset" class="fa-fw"/>
                         <span class="dashboard-attention-label">{{ trans('general.dashboard_pending_requests') }}</span>
                         <span class="badge dashboard-attention-count">{{ number_format($pendingRequestsCount) }}</span>
                     </a>
                 </li>
             @endcanany
-            @can('view', \App\Models\License::class)
-                <li class="list-group-item">
-                    <a href="{{ route('licenses.index') }}">
-                        <span class="dashboard-attention-label">{{ trans('general.dashboard_licenses_expiring_soon') }}</span>
-                        <span class="badge dashboard-attention-count">{{ number_format($licensesExpiringSoon) }}</span>
-                    </a>
-                </li>
-            @endcan
+
             @can('view', \App\Models\Asset::class)
                 <li class="list-group-item">
                     <a href="{{ route('hardware.index') }}">
+                        <x-icon type="asset" class="fa-fw"/>
                         <span class="dashboard-attention-label">{{ trans('general.dashboard_assets_past_eol') }}</span>
                         <span class="badge dashboard-attention-count">{{ number_format($assetsPastEol) }}</span>
+                    </a>
+                </li>
+                <li class="list-group-item">
+                    <a href="{{ route('maintenances.index', ['upcoming_status' => 'overdue']) }}">
+                        <x-icon type="maintenances" class="fa-fw"/>
+                        <span class="dashboard-attention-label">{{ trans('general.dashboard_overdue_maintenances') }}</span>
+                        <span class="badge dashboard-attention-count">{{ number_format($overdueMaintenances) }}</span>
+                    </a>
+                </li>
+            @endcan
+            @can('view', \App\Models\License::class)
+                <li class="list-group-item">
+                    <a href="{{ route('licenses.index') }}">
+                        <x-icon type="license" class="fa-fw"/>
+                        <span class="dashboard-attention-label">{{ trans('general.dashboard_licenses_expiring_soon') }}</span>
+                        <span class="badge dashboard-attention-count">{{ number_format($licensesExpiringSoon) }}</span>
                     </a>
                 </li>
             @endcan
             @can('view', \App\Models\User::class)
                 <li class="list-group-item">
                     <a href="{{ route('users.index') }}">
+                        <x-icon type="users" class="fa-fw"/>
                         <span class="dashboard-attention-label">{{ trans('general.dashboard_users_offboarding_soon') }}</span>
                         <span class="badge dashboard-attention-count">{{ number_format($usersOffboardingSoon) }}</span>
                     </a>
