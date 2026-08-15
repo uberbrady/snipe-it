f767340c18a0b1d3a928ef954102bc4fe5eb97be
snipe <snipe@snipe.net>
2026-08-15 10:11:16 +0100

    Temp removed calendar button

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -568,14 +568,7 @@
                                 </li>
                             @endif
 
-                            @can('view', \App\Models\Asset::class)
-                                <li{!! (request()->routeIs('calendar.index') ? ' class="active" aria-current="page"' : '') !!}>
-                                    <a href="#">
-                                        <x-icon type="calendar" class="fa-fw"/>
-                                        <span>{{ trans('general.calendar') }}</span>
-                                    </a>
-                            </li>
-                        @endcan
+                            
                         @can('view', \App\Models\License::class)
                             <li{!! (request()->is('licenses*') ? ' class="active" aria-current="page"' : '') !!}>
                                 <a href="{{ route('licenses.index') }}">
