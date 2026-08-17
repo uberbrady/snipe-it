87f714cb6748bfe073fd6d43acc75933eb028021
snipe <snipe@snipe.net>
2026-08-17 11:42:21 +0100

    Bumped minor version

--- config/version.php
+++ config/version.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'app_version' => 'v8.7.0',
-    'full_app_version' => 'v8.7.3-pre - build 24424-gd3be770f25',
-    'build_version' => '24424',
+    'app_version' => 'v8.7.1',
+    'full_app_version' => 'v8.7.1 - build 24503-g687a3797c1',
+    'build_version' => '24503',
     'prerelease_version' => '',
-    'hash_version' => 'gd3be770f25',
-    'full_hash' => 'v8.7.0-1179-gd3be770f25',
+    'hash_version' => 'g687a3797c1',
+    'full_hash' => 'v8.7.1-90-g687a3797c1',
     'branch' => 'develop',
 ];
