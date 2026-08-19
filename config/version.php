f7090acc9927778b44741440570a643bd4e06d30
snipe <snipe@snipe.net>
2026-08-19 21:12:32 +0100

    Bumped version to 8.7.2

--- config/version.php
+++ config/version.php
@@ -1,11 +1,11 @@
 <?php
 
 return [
-    'app_version' => 'v8.7.1',
-    'full_app_version' => 'v8.7.1 - build 24503-g687a3797c1',
-    'build_version' => '24503',
+    'app_version' => 'v8.7.2',
+    'full_app_version' => 'v8.7.2 - build 24589-gf0bd1f8d76',
+    'build_version' => '24589',
     'prerelease_version' => '',
-    'hash_version' => 'g687a3797c1',
-    'full_hash' => 'v8.7.1-90-g687a3797c1',
+    'hash_version' => 'gf0bd1f8d76',
+    'full_hash' => 'v8.7.2-100-gf0bd1f8d76',
     'branch' => 'develop',
 ];
