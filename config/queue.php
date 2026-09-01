5ca5fa7b604a02f8d95e219f8eb5fd7435f29e6e
Brady Wetherington <bwetherington@grokability.com>
2026-09-01 11:14:14 +0100

    Change the 'default' queue back to 'sync' :/

--- config/queue.php
+++ config/queue.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => env('QUEUE_CONNECTION', 'background'),
+    'default' => env('QUEUE_CONNECTION', 'sync'),
 
     /*
     |--------------------------------------------------------------------------
